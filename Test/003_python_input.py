113c9b33e52a96faec164bd6258c1f2f7f153bb6
eaindome <ekow.a.indome@gmail.com>
2023-05-15 15:52:07 -1200

    Deleted

--- Test/003_python_input.py
+++
@@ -1,8 +0,0 @@
-# trying to receive input from the user
-
-# input function
-#input("What is your name? ")
-
-# input() will get user input in console
-# Then print() will print the word "Hello" and the user input
-print("Hello "+input("What is your name?\n")+"!")
--- Test/004_variables.py
+++
@@ -1,8 +0,0 @@
-## Variables
-
-# saving data with variables
-name = input("What is your name?\n")
-print(name)
-
-length = len(name)
-print(length)
