96fee180d9bd7dfa46838071f42539db633bbd35
eaindome <ekow.a.indome@gmail.com>
2023-05-18 23:04:04 -1200

    Small change in alphabet

--- Python/100 Days of Code/Day 8/Caesar_Cipher-Part1.py
+++ Python/100 Days of Code/Day 8/Caesar_Cipher-Part1.py
@@ -1,7 +1,8 @@
 # Caesar Cipher 1
 
 # Don't change the code below
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt: \n")
 text = input("Type your message: \n")
