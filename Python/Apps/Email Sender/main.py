874d86626bcf8ff95f172ceb8bf7376446044c4a
eaindome <ekow.a.indome@gmail.com>
2023-06-07 22:02:55 -1200

    making a few changes

--- Python/Apps/Email Sender/main.py
+++ Python/Apps/Email Sender/main.py
@@ -27,7 +27,7 @@ em.set_content(body)
 
 context = ssl.create_default_context()
 
-with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context):
+with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
     smtp.login(email_sender, email_password)
     smtp.sendmail(email_sender, email_receiver, em.as_string())
 
