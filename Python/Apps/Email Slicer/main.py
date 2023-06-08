b248b867f0c7d45e714098e8ff372e785471470a
eaindome <ekow.a.indome@gmail.com>
2023-06-07 22:59:28 -1200

    Email Slicer code

---
+++ Python/Apps/Email Slicer/main.py
@@ -0,0 +1,22 @@
+# collect email from user
+# split the email using the @,  the first part as the user name, the second part
+# is gonna be saved as domain 
+# split domain using .,
+
+def main():
+    print("Welcome to the email slicer")
+    print("")
+
+    email_input = input("Input your email address: ")
+
+    (username, domain) = email_input.split("@")
+    (domain, extension) = domain.split(".")
+
+    print(f"Username: {username}")
+    print(f"Domain: {domain}")
+    print(f"Extension: {extension}")
+
+while True:
+    main()
+
+
