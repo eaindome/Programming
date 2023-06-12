ddd840765731847d8b392e0c540a5aebe6af910d
eaindome <ekow.a.indome@gmail.com>
2023-06-11 12:56:49 -1200

    variables and constants in javascript

---
+++ Java Script/Programiz/variables-and-constants.js
@@ -0,0 +1,33 @@
+// here, num is a variable storing 5
+let num = 5
+
+// javascript declare variables
+// we use either 'var' or 'let' keyword to declare variable
+var x;
+let y;
+
+// javascript initialize variables
+// the '=' operator is used to assign a value to a variable
+let x;      // x is the name of the variable
+x = 5;
+
+let a = 5;
+let b = 6;
+
+let j = 5, k = 6, l = 7;
+
+
+// change the value of variables
+// 5 is assigned to variable x
+let x = 5;
+console.log(x); // 5
+
+// value of variable x is changed
+x = 3;
+console.log(x); // 3
+
+// javascript constants
+// once a constant is initialized, we cannot change its value
+const x = 5;
+x = 10; // Error! constant cannot be changed
+console.log(x)
