db0f22b833dd75c6aad7fcd01f43c8bf6d669ddc
eaindome <ekow.a.indome@gmail.com>
2023-06-18 23:32:16 -1200

    Routes for user'

--- Projects/Class Finder/Test 2/src/user/routes.js
+++ Projects/Class Finder/Test 2/src/user/routes.js
@@ -3,6 +3,7 @@ const controller = require('./controller');
 
 const router = Router();
 
-
+router.post("/", controller.userLogin);
+router.get("/", controller.userProfile);
 
 module.exports = router;
