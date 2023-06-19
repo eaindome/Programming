d6c6f6a30691a5b96626d49d263edbb7e26f541f
eaindome <ekow.a.indome@gmail.com>
2023-06-18 23:30:29 -1200

    database script

--- Projects/Class Finder/Test 2/database.js
+++ Projects/Class Finder/Test 2/database.js
@@ -3,7 +3,7 @@ const Pool =require("pg").Pool;
 const pool = new Pool({
     user: "postgres",
     host: "localhost",
-    database: "class_finder_test2",
+    database: "class_finder_test3",
     password: "Eai@2460",
     port: 5432,
 });
