8602dc34e1d17980c4e26ef4c9db3716d9fa95eb
eaindome <ekow.a.indome@gmail.com>
2023-06-19 21:37:32 -1200

    Test 2

--- Projects/Class Finder/Test 2/server.js
+++ Projects/Class Finder/Test 2/server.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const bodyParser = require('body-parser');
 const session = require('express-session');
-const userRoutes = require('./src/user/routes')
-const classRoutes = require('./src/classes/routes')
-//const upcomingClasses = require('./src/classes/upcomingClasses/routes');
+const userRoutes = require('./src/user/routes');
+const classRoutes = require('./src/classes/routes');
 const classStatus = require('./src/classes/classStatus/routes');
 const upcomingClasses = require('./src/classes/upcomingClasses/routes');
+const updateClassStatus = require('./src/classes/updateClassStatus/routes');
+const bookClass = require('./src/classes/bookClass/routes');
 
 const app = express();
 const port = 3000;
@@ -26,6 +27,8 @@ app.use("/api/v1/src/user", userRoutes);
 app.use("/api/v1/src/classes", classRoutes);
 app.use("/api/v1/src/classes/classStatus", classStatus);
 app.use("/api/v1/src/classes/upcomingClasses", upcomingClasses);
+app.use("/api/v1/src/classes/updateClassStatus", updateClassStatus);
+app.use("/api/v1/src/classes/bookClass", bookClass);
 
 
 // Start the server
@@ -33,3 +36,12 @@ app.listen(port, () => {
     console.log(`Server is running on port ${port}.`)
 });
 
+// Trigger the class status update every 5 minutes (adjust the interval as needed)
+setInterval(() => {
+    // Import the controller function
+    const { updateClassStatus } = require('./src/classes/updateClassStatus/controller');
+  
+    // Call the class status update function
+    updateClassStatus();
+  }, 
+  5 * 60 * 1000); // 5 minutes interval
