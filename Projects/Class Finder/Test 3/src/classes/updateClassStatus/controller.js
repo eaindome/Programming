4a485457f3cc39c13e90fb9212ceb4cac1e7cfc6
eaindome <ekow.a.indome@gmail.com>
2023-06-20 21:50:16 -1200

    Updating class status scripts

--- Projects/Class Finder/Test 3/src/classes/updateClassStatus/controller.js
+++ Projects/Class Finder/Test 3/src/classes/updateClassStatus/controller.js
@@ -1,43 +1,63 @@
 const pool = require('../../../database');
 const queries = require('./queries');
 
-// Endpoint: Update Class Status
-const updateClassStatus = async () => {
-  const currentDay = new Date().getDay(); // Get the current day of the week (0 = Sunday, 1 = Monday, etc.)
+// Update class status for multiple rooms
+const updateClassStatus = async (status, roomIds) => {
+  await pool.query(queries.updateClassStatus, [status, roomIds]);
+};
+
+// Update class status for all rooms
+const updateAllClassStatus = async (status) => {
+  await pool.query(queries.updateAllClassStatus, [status]);
+};
+
+// Update room statuses based on timetable data
+const updateRoomStatuses = async () => {
+  const currentDay = new Date().getDay();
+  const currentTime = new Date().toLocaleTimeString('en-US', { hour12: false });
 
   // Check if it's Sunday (0) or Saturday (6)
   if (currentDay === 0 || currentDay === 6) {
-    // If it's Sunday or Saturday, set the class status to "Empty" for all timetables
-    await pool.query(queries.updateAllClassStatus, ['Empty']);
+    // If it's Sunday or Saturday, set the class status to "Empty" for all rooms
+    await updateAllClassStatus('Available');
     return;
   }
 
-  const currentTime = new Date().toLocaleTimeString('en-US', { hour12: false }); // Get the current time in 24-hour format (e.g., "12:00:00")
-
-  // Get the timetables that are currently ongoing
+  // Get the ongoing timetables
   const ongoingTimetables = await pool.query(queries.getOngoingTimetables, [currentDay, currentTime]);
-
   const ongoingTimetableIds = ongoingTimetables.rows.map((timetable) => timetable.timetable_id);
 
   if (ongoingTimetableIds.length > 0) {
     // Set the class status to "Ongoing" for the ongoing timetables
-    await pool.query(queries.updateClassStatus, ['Ongoing', ongoingTimetableIds]);
+    await updateClassStatus('Ongoing', ongoingTimetableIds);
   }
 
-  // Get the timetables that are not ongoing
+  // Get the non-ongoing timetables
   const nonOngoingTimetables = await pool.query(queries.getNonOngoingTimetables, [currentDay, currentTime]);
-
   const nonOngoingTimetableIds = nonOngoingTimetables.rows.map((timetable) => timetable.timetable_id);
 
   if (nonOngoingTimetableIds.length > 0) {
     // Set the class status to "Empty" for the non-ongoing timetables
-    await pool.query(queries.updateClassStatus, ['Empty', nonOngoingTimetableIds]);
+    await updateClassStatus('Available', nonOngoingTimetableIds);
   }
 
   // Log the updated class statuses
   console.log('Class statuses updated at:', currentTime);
 };
 
+// Automatically update room statuses at regular intervals
+const interval = setInterval(async () => {
+  await updateRoomStatuses();
+}, 6 * 60 * 1000); // Update every 5 minutes or if you want for 1 minute (60000)
+
+// Stop updating room statuses when the application exits
+process.on('SIGINT', () => {
+  clearInterval(interval);
+  process.exit(0);
+});
+
+
+
 module.exports = {
-  updateClassStatus,
+  updateRoomStatuses,
 };
--- Projects/Class Finder/Test 3/src/classes/updateClassStatus/routes.js
+++ Projects/Class Finder/Test 3/src/classes/updateClassStatus/routes.js
@@ -3,6 +3,6 @@ const classController = require('./controller');
 
 const router = Router();
 
-router.get('/update-class-status', classController.updateClassStatus);
+router.get('/', classController.updateRoomStatuses);
 
 module.exports = router;
