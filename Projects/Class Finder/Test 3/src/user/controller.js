9728321bb1341869e478881283f70155749c506d
eaindome <ekow.a.indome@gmail.com>
2023-06-23 19:27:27 -1200

    Updating scripts

--- Projects/Class Finder/Test 3/src/user/controller.js
+++ Projects/Class Finder/Test 3/src/user/controller.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcrypt');
+//const bcrypt = require('bcrypt');
 const pool = require('../../database');
 const queries = require('./queries');
 
@@ -21,6 +21,7 @@ const userLogin = (req, res) => {
         }
     });
 };
+
 
 //  Endpoint: User Profile
 const userProfile = (req, res) => {
@@ -47,6 +48,7 @@ const userProfile = (req, res) => {
     });
 };
 
+
 // Endpoint: User Logout
 const userLogout = (req, res) => {
     // Destroy the session
@@ -60,100 +62,8 @@ const userLogout = (req, res) => {
     });
 };
 
-// Endpoint: User Sign Up
-const userSignUp = async (req, res) => {
-    const { firstname, surname, password, email, program, year } = req.body;
-  
-    try {
-      // Check if the user already exists in the database
-      const userExists = await pool.query(queries.checkUserExists, [email]);
-      if (userExists.rowCount > 0) {
-        return res.status(409).json({ error: 'User already exists' });
-      }
-  
-      // Generate the hashed password
-      const hashedPassword = await bcrypt.hash(password, 10);
-  
-      // Insert the user into the Users table
-      const programYearId = await getProgramYearId(program, year);
-      const user = await pool.query(queries.userSignUp, [
-        `${firstname} ${surname}`,
-        hashedPassword,
-        email,
-        programYearId
-      ]);
-  
-      res.status(201).json({ message: 'User created successfully' });
-    } catch (error) {
-      console.error('Error executing query: ', error);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
-  };
-  
-  // Helper function to get program_year_id
-  const getProgramYearId = async (program, year) => {
-    try {
-      const programYear = await pool.query(queries.getProgramYear, [
-        program,
-        year
-      ]);
-  
-      if (programYear.rowCount > 0) {
-        return programYear.rows[0].program_year_id;
-      }
-  
-      // If program_year_id does not exist, insert a new entry into ProgramYears table
-      const programId = await getProgramId(program);
-      const yearId = await getYearId(year);
-  
-      const newProgramYear = await pool.query(queries.insertProgramYear, [
-        programId,
-        yearId
-      ]);
-  
-      return newProgramYear.rows[0].program_year_id;
-    } catch (error) {
-      throw error;
-    }
-  };
-  
-  // Helper function to get program_id
-  const getProgramId = async (program) => {
-    try {
-      const programData = await pool.query(queries.getProgram, [program]);
-  
-      if (programData.rowCount > 0) {
-        return programData.rows[0].program_id;
-      }
-  
-      throw new Error(`Program ${program} not found`);
-    } catch (error) {
-      throw error;
-    }
-  };
-  
-  // Helper function to get year_id
-  const getYearId = async (year) => {
-    try {
-      const yearData = await pool.query(queries.getYear, [year]);
-  
-      if (yearData.rowCount > 0) {
-        return yearData.rows[0].year_id;
-      }
-  
-      throw new Error(`Year ${year} not found`);
-    } catch (error) {
-      throw error;
-    }
-  };
-
-
-
-
-
 module.exports = {
     userLogin,
     userProfile,
     userLogout,
-    userSignUp,
 };
--- Projects/Class Finder/Test 3/src/user/routes.js
+++ Projects/Class Finder/Test 3/src/user/routes.js
@@ -3,9 +3,9 @@ const controller = require('./controller');
 
 const router = Router();
 
+
 router.post("/", controller.userLogin);
 router.get("/", controller.userProfile);
 router.post("/logout", controller.userLogout);
-router.post("/signup", controller.userSignUp);
 
 module.exports = router;
