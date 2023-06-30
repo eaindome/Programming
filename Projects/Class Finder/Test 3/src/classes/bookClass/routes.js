32aa311a43a3c3d70186e47d77e79dbbbd9c9c8e
eaindome <ekow.a.indome@gmail.com>
2023-06-29 20:11:34 -1200

    updating routes script

--- Projects/Class Finder/Test 3/src/classes/bookClass/routes.js
+++ Projects/Class Finder/Test 3/src/classes/bookClass/routes.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const { bookClass, cancelRoomBooking } = require('./controller');
+const { bookClass, cancelRoomBooking, getAvailableTimes, getAvailableTimesCurrent } = require('./controller');
 
 const router = express.Router();
 
 router.post('/book/:roomId', bookClass);
 router.put('/cancel/:roomId', cancelRoomBooking);
+router.get('/availableTimes/:roomId/:day', getAvailableTimes);
+router.get('/getAvailableTimesCurrent/:roomId', getAvailableTimesCurrent);
 
 module.exports = router;
 
