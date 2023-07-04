d47994b5756f95dc39ffd143bd93179a82bd4e8b
eaindome <ekow.a.indome@gmail.com>
2023-07-04 07:59:01 -1200

    updating server.js

--- Projects/Class Finder/Test 3/server.js
+++ Projects/Class Finder/Test 3/server.js
@@ -20,6 +20,7 @@ const searchLectureRooms = require('./src/classes/searchClasses/routes');
 const notificationRoutes = require('./src/notifications/routes');
 const { sendNotificationsForUpcomingClasses } = require('./src/notifications/controller');
 const favorites = require('./src/favourites/routes');
+const forgotPassword = require('./src/user/userforgetpassword/routes');
 
 
 const app = express();
@@ -69,7 +70,8 @@ app.use("/api/v1/src/classes/ongoingClasses", ongoingSessions);
 app.use("/api/v1/src/classes/availableClasses", availableLectureRooms);
 app.use("/api/v1/src/classes/searchClasses", searchLectureRooms);
 app.use('/api/v1/src/notifications', notificationRoutes);
-app.use("/api/v1/src/favourites", favorites)
+app.use("/api/v1/src/favourites", favorites);
+app.use("/api/v1/src/user/userforgotpassword", forgotPassword);
 
 // Set up the scheduled task
 cron.schedule('* * * * *', sendNotificationsForUpcomingClasses);
