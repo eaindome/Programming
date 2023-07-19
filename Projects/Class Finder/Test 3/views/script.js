12359cc7098599de9f336814f6e54df96b32db5d
eaindome <ekow.a.indome@gmail.com>
2023-07-18 19:36:56 -1200

    view files

--- Projects/Class Finder/Test 3/views/script.js
+++ Projects/Class Finder/Test 3/views/script.js
@@ -2,24 +2,24 @@
 
 // Function to validate the password fields
 const validatePassword = () => {
-    const passwordField = document.getElementById('password');
-    const confirmPasswordField = document.getElementById('confirmPassword');
-    const errorMessage = document.getElementById('error-message');
+  const passwordField = document.getElementById('password');
+  const confirmPasswordField = document.getElementById('confirmPassword');
+  const errorMessage = document.getElementById('error-message');
   
-    if (passwordField.value !== confirmPasswordField.value) {
-      errorMessage.textContent = 'Passwords do not match';
-      return false;
-    }
+  if (passwordField.value !== confirmPasswordField.value) {
+    errorMessage.textContent = 'Passwords do not match';
+    return false;
+  }
   
-    errorMessage.textContent = '';
-    return true;
-  };
+  errorMessage.textContent = '';
+  return true;
+};
   
-  // Attach the event listener to the form submission
-  const form = document.getElementById('reset-form');
-  form.addEventListener('submit', (event) => {
-    if (!validatePassword()) {
-      event.preventDefault();
-    }
+// Attach the event listener to the form submission
+const form = document.getElementById('reset-form');
+form.addEventListener('submit', (event) => {
+  if (!validatePassword()) {
+    event.preventDefault();
+  }
 });
   
