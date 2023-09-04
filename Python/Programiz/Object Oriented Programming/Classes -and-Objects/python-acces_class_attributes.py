b716977de8338663c4f87f5a1035f14cef6c004a
ekow <ekow.a.indome@gmail.com>
2023-09-04 21:16:56 +0000

    Classes and Objects

---
+++ Python/Programiz/Object Oriented Programming/Classes -and-Objects/python-acces_class_attributes.py
@@ -0,0 +1,24 @@
+# Python Objects
+'''
+An object is called an instance of a class. For example, suppose Bike is a class then we can create objects like bike1, bike2, etc from the class.
+'''
+
+# Syntax
+'''
+objectName = ClassName()
+'''
+
+# create class
+class Bike:
+    name = ""
+    gear = 0
+
+# create objects of class
+bike1 = Bike()
+
+# accessing class attributes using objects
+# modify the name attribute
+bike1.name = "Mountain Bike"
+
+# access the gear attribute
+bike1.gear
---
+++ Python/Programiz/Object Oriented Programming/Classes -and-Objects/python-constructors.py
@@ -0,0 +1,12 @@
+'''
+Initializing values using the constructors
+
+'__init__' is the constructor functions
+'''
+class Bike:
+
+    # constructor function
+    def __init__(self, name =""):
+        self.name = name
+
+bike1 = Bike("Mountain Bike")
