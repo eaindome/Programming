8511d990b9ab233a1678297be2340690a5c91dcb
ekow <ekow.a.indome@gmail.com>
2023-09-05 22:51:06 +0000

    Day 1

--- Python/100 Days of Code/Day 16-Object Oriented Programming/001.py
+++ Python/100 Days of Code/Day 16-Object Oriented Programming/001.py
@@ -3,7 +3,14 @@
 # AJ = turtle.Turtle()
 
 
-from turtle import Turtle
-AJ = Turtle(
+from turtle import Turtle, Screen
+Leo = Turtle()
+print(Leo)
+Leo.shape("turtle")
+Leo.color("DarkGreen")
+Leo.forward(100)
 
 
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
