c7ff185eb51f134e32e08af275ac5e93f82be713
ekow <ekow.a.indome@gmail.com>
2023-09-11 11:22:15 +0000

    Updating it to using modules

---
+++ ExpressJs/src/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const groceriesRoute = require('./routes/groceries');
+const marketRoute = require('./routes/markets');
+
+const app = express();
+const PORT = 3001;
+
+// Middleware
+app.use(express.json());
+
+// global middleware function for requests
+app.use((req, res, next) => {
+    console.log(req.url);
+    console.log(`${req.method}: ${req.url}`);
+    next();
+});
+
+// using the routes in the routes folder
+// using prefix for the routes
+app.use('/api/groceries', groceriesRoute);
+app.use('/api/markets', marketRoute);
+
+app.listen(PORT, () => console.log(`Running Express Server on Port ${PORT}`));
+
+
+
+
