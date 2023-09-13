99c76f5b1182486d28047d08b521b4a74ef5429e
ekow <ekow.a.indome@gmail.com>
2023-09-13 10:21:08 +0000

    database connection for the finance management

--- Projects/Personal-Finance-Management-and-Budgeting/BackEnd/databse.js
+++ Projects/Personal-Finance-Management-and-Budgeting/BackEnd/databse.js
@@ -3,7 +3,7 @@ const Pool = require("pg").Pool;
 const pool = new Pool({
     user: "postgres",
     host: "localhost",
-    database: "",
+    database: "financemanagement",
     password: "Eai@2460",
     port: 5432,
 });
