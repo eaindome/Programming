8ab62baba9ed9da7087bd46c300f6a9a3c5b92d8
ekow <ekow.a.indome@gmail.com>
2023-09-18 00:21:43 +0000

    updating login and refresh routes

--- NodeJS/API Authentication/Routes/auth.routes.js
+++ NodeJS/API Authentication/Routes/auth.routes.js
@@ -3,7 +3,8 @@ const router = express.Router();
 const createError = require('http-errors');
 const User = require('../Models/user.model');
 const { authSchema } = require('../helpers/validation_schema');
-const { signAccessToken } = require('../helpers/jwt_helper');
+const { signAccessToken, signRefreshToken, verifyRefreshToken } = require('../helpers/jwt_helper');
+const { verify } = require('jsonwebtoken');
 
 // route for registration or signup process
 router.post('/register', async (req, res, next) => {
@@ -20,8 +21,9 @@ router.post('/register', async (req, res, next) => {
         const user = new User(result);              // User({ email,password });
         const savedUser = await user.save();
         const accessToken = await signAccessToken(savedUser.id);
+        const refreshToken = await signRefreshToken(savedUser.id);
 
-        res.send({accessToken});
+        res.send({ accessToken, refreshToken });
     } catch (error) {
         if (error.isJoi === true) error.status = 422;
         next(error);
@@ -30,12 +32,43 @@ router.post('/register', async (req, res, next) => {
 
 // route for login process
 router.post('/login', async (req, res, next) => {
-    res.send('login route')
+    // res.send('login route')
+    try{
+        const result = await authSchema.validateAsync(req.body);
+        const user = await User.findOne({ email: result.email });
+
+        if (!user) throw createError.NotFound('User not registered.');
+
+        const isMatch = await user.isValidPassword(result.password);
+        if (!isMatch) throw createError.Unauthorized('Username/Password is not valid');
+
+        const accessToken = await signAccessToken(user.id);
+        const refreshToken = await signRefreshToken(user.id);
+
+        res.send({ accessToken, refreshToken });
+    } catch (error) {
+        if (error.isJoi === true) 
+            return next(createError.BadRequest('Invalid Username/Password'));
+        next(error);
+    }
 });
 
 // route to send refresh token
 router.post('/refresh-token', async (req, res, next) => {
-    res.send('refresh-token route')
+    // res.send('refresh-token route')
+    try {
+        const { refreshToken } = req.body;
+
+        if (!refreshToken) throw createError.BadRequest();
+        const userId = await verifyRefreshToken(refreshToken);
+
+        const accessToken = await signAccessToken(userId);
+        const refToken = await signRefreshToken(userId);
+
+        res.send({ accessToken: accessToken, refreshToken: refToken });
+    } catch (error) {
+        next(error);
+    }
 });
 
 // route for logout process
