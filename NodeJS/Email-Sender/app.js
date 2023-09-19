0b2c4865194524c8560c4f8d6b238c616d658247
Ekow <91199308+eaindome@users.noreply.github.com>
2023-09-19 23:15:05 +0000

    Update app.js

--- NodeJS/Email-Sender/app.js
+++ NodeJS/Email-Sender/app.js
@@ -1,10 +1,10 @@
 const nodemailer = require('nodemailer');
 const { google } = require('googleapis');
 
-const CLIENT_ID = '318203718345-mqoi3p02kl7nevi6m08gnimtm1gvfmak.apps.googleusercontent.com';
-const CLIENT_SECRET = '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';
-const REDIRECT_URL = 'https://developers.google.com/oauthplayground';
-const REFRESH_TOKEN = '1//04gi-VL7z0lUuCgYIARAAGAQSNwF-L9IrQNdCdZvuhu79JAhlaVruTS48UrGW8DXa1mzGjl-Fw58IsLjx5DR3JEBKY8sjLMpdWPs';
+const CLIENT_ID = 'your-client-id';
+const CLIENT_SECRET = 'your-client-secret';
+const REDIRECT_URL = 'redirect-url';
+const REFRESH_TOKEN = 'refresh-token';
 
 const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URL);
 oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
@@ -26,8 +26,8 @@ async function sendMail() {
         });
 
         const mailOptions = {
-            from: 'ekow <ekow.a.indome@gmail.com',
-            to: 'eaindome@gmail.com',
+            from: 'ekow <your-email',
+            to: 'recepient-email',
             subject: "Hello from gmail using API",
             text: "Hello from gmail email useing API",
             html: '<h1>Hello from gmail email using API</h1>'
