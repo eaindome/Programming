b4f75c0b93458887acb6c6fc599fb9d29f3e9023
ekow <ekow.a.indome@gmail.com>
2023-09-21 22:36:08 +0000

    server / client side connection

---
+++ Projects/Pizza Delivery/main.py
@@ -0,0 +1,15 @@
+from auth_routes import auth_router
+from fastapi import FastAPI
+from fastapi_jwt_auth import AuthJWT
+from order_routes import order_router
+from schemas import Settings
+
+
+app = FastAPI()
+
+@AuthJWT.load_config
+def get_config():
+    return Settings()
+
+app.include_router(auth_router)
+app.include_router(order_router)
