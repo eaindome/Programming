a42ae1de34a62ed4453109556cb867487a770840
ekow <ekow.a.indome@gmail.com>
2023-09-22 21:56:30 +0000

    updating database schemas

--- Projects/Pizza Delivery/order_routes.py
+++ Projects/Pizza Delivery/order_routes.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter, HTTPException, status, Depends
 from fastapi_jwt_auth import AuthJWT
 from fastapi.encoders import jsonable_encoder
 from models import User, Order
-from schemas import OrderModel
+from schemas import OrderModel, OrderStatusModel
 
 
 order_router = APIRouter(
@@ -22,6 +22,7 @@ def authorization(Authorize: AuthJWT=Depends()):
             detail="Invalid username or password"
         )
 
+# as sample hello world
 @order_router.get('/', status_code=status.HTTP_200_OK)
 async def hello(Authorize: AuthJWT=Depends()):
     authorization(Authorize)
@@ -30,6 +31,12 @@ async def hello(Authorize: AuthJWT=Depends()):
 # make an order
 @order_router.post('/', status_code=status.HTTP_200_OK)
 async def place_an_order(order: OrderModel, Authorize: AuthJWT=Depends()):
+    """
+        # Placing / Creating an order
+        Required Fields:
+            i. quantity : integer
+            ii. pizza size : string
+    """
     try:
         Authorize.jwt_required()
     except Exception as e:
@@ -61,8 +68,12 @@ async def place_an_order(order: OrderModel, Authorize: AuthJWT=Depends()):
     return jsonable_encoder(response)
 
 # get a list of orders made
-@order_router.get('/orders', status_code=status.HTTP_200_OK)
+@order_router.get('/order', status_code=status.HTTP_200_OK)
 async def get_list_of_orders(Authorize: AuthJWT=Depends()):
+    """
+        # List all orders
+        List all orders made to the firm. Can only be accessed by staff
+    """
     authorization(Authorize)
 
     current_user=Authorize.get_jwt_subject()
@@ -78,9 +89,13 @@ async def get_list_of_orders(Authorize: AuthJWT=Depends()):
         detail="You are not a staff"
     )
 
-# get a list of orders
-@order_router.get('/orders/{order_id}', status_code=status.HTTP_200_OK)
+# get an orders
+@order_router.get('/order/{order_id}', status_code=status.HTTP_200_OK)
 async def retrieve_an_order(order_id: int, Authorize: AuthJWT=Depends()):
+    """
+        # Get an order by id
+        Only accessible to staff 
+    """
     authorization(Authorize=Authorize)
 
     user = Authorize.get_jwt_subject()
@@ -98,6 +113,10 @@ async def retrieve_an_order(order_id: int, Authorize: AuthJWT=Depends()):
 # get user's orders
 @order_router.get('/user/orders', status_code=status.HTTP_200_OK)
 async def get_user_orders(Authorize: AuthJWT=Depends()):
+    """
+        # Get current user's list of orders
+        Accessible by users
+    """
     authorization(Authorize)
 
     user = Authorize.get_jwt_subject()
@@ -107,10 +126,14 @@ async def get_user_orders(Authorize: AuthJWT=Depends()):
 
 # get a specific order
 @order_router.get(
-    '/user/orders/{order_id}',
+    '/user/order/{order_id}',
     status_code=status.HTTP_200_OK
 )
 async def get_specific_order(order_id: int, Authorize: AuthJWT=Depends()):
+    """
+        # Get a specific order by the current user
+        Accessible to Users
+    """
     authorization(Authorize)
 
     user = Authorize.get_jwt_subject()
@@ -125,6 +148,71 @@ async def get_specific_order(order_id: int, Authorize: AuthJWT=Depends()):
         detail="Order does not exist."
     )
 
+# update an order
+@order_router.put('/order/update/{order_id}')
+async def update_order(order_id: int, order: OrderModel, Authorize: AuthJWT=Depends()):
+    """
+        # Update a specific order
+        Accessible to users
+        Fields required:
+            - quantity : integer
+            - pizza sizes : string
+    """
+    authorization(Authorize=Authorize)
+
+    order_update=session.query(Order).filter(Order.id==order_id).first()  
+
+    order_update.quantity = order.quantity
+    order_update.pizza_sizes = order.pizza_sizes
+
+    session.commit()
+
+    return jsonable_encoder(order_update)
+
+
+# update an order status
+@order_router.patch('/order/update/{order_id}')
+async def update_order_status(order_id: int, order: OrderStatusModel, Authorize: AuthJWT=Depends()):
+    """
+        # Update an order's status
+        Accessible to staff
+        Required Fields:
+            - order status : string
+    """
+    authorization(Authorize=Authorize)
+
+    user = Authorize.get_jwt_subject()
+    current_user = session.query(User).filter(User.username==user).first()
+
+    if current_user.is_staff:
+        order_stats_update=session.query(Order).filter(Order.id==order_id).first()
+
+        order_stats_update.order_status=order.order_status
+
+        session.commit()
+        response = {
+            "order_id": order_stats_update.id,
+            "quantity": order_stats_update.quantity,
+            "pizza_size": order_stats_update.pizza_sizes,
+            "order_status": order_stats_update.order_status
+        }
+
+        return jsonable_encoder(response)
+
+# delete an order
+@order_router.delete('/order/delete/{order_id}', status_code=status.HTTP_204_NO_CONTENT)
+async def delete_an_order(order_id:int, Authorize: AuthJWT=Depends()):
+    """
+        # Deletes an order by id
+        Accessible to everyone
+    """
+    authorization(Authorize=Authorize)
+
+    delete_order = session.query(Order).filter(Order.id==order_id).first()
+    session.delete(delete_order)
+    session.commit()
+
+    return delete_order
 
 
 
