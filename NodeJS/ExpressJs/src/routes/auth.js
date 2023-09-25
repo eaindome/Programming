1525fc6a6b15b6dea686ef562198d79f8dcba079
ekow <ekow.a.indome@gmail.com>
2023-09-25 21:20:19 +0000

    adding sign up route

--- NodeJS/ExpressJs/src/routes/auth.js
+++ NodeJS/ExpressJs/src/routes/auth.js
@@ -1,9 +1,11 @@
 const { Router } = require('express');
+const User = require('../database/schemas/user_schemas');
+const { hashPassword } = require('./helpers');
 
 const router = Router();
 
 
-router.post('/', (request, response) => {
+router.post('/login', (request, response) => {
     const { username, password } = request.body;
     if (username && password) {
         if (request.session.user) {
@@ -15,6 +17,18 @@ router.post('/', (request, response) => {
             response.send(request.session);
         }
     } else response.send(401); 
+});
+
+router.post('/register', async (request, response) => {
+    const { username, password, email } = request.body;
+    const userDB = await User.findOne({ $or: [{ username }, { email }] });
+    if (userDB) {
+        response.status(400).send({ msg: 'User already existes!' });
+    } else {
+        const password = hashPassword(request.body.password);
+        const newUser = User.create({ username, password, email });
+        response.send(201);
+    }
 });
 
 module.exports = router;
