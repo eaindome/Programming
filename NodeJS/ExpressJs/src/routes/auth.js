00640629989a6535ee123fba2e8e883b44069924
ekow <ekow.a.indome@gmail.com>
2023-09-26 20:54:47 +0000

    updating login router with hashed password functionality

--- NodeJS/ExpressJs/src/routes/auth.js
+++ NodeJS/ExpressJs/src/routes/auth.js
@@ -1,27 +1,29 @@
 const { Router } = require('express');
 const User = require('../database/schemas/user_schemas');
-const { hashPassword } = require('./helpers');
+const { hashPassword, comparePassword } = require('./helpers');
 
 const router = Router();
 
 
-router.post('/login', (request, response) => {
-    const { username, password } = request.body;
-    if (username && password) {
-        if (request.session.user) {
-            response.send(response.session.user);
-        } else {
-            request.session.user = {
-                username,
-            };
-            response.send(request.session);
-        }
-    } else response.send(401); 
+router.post('/login', async (request, response) => {
+    const { email, password } = request.body;
+    if (!email || !password) return response.status(400);
+    const userDB = await User.findOne({ email })
+    if (!userDB) return response.send(401);
+    const isValid = comparePassword(password, userDB.password);
+    if (isValid) {
+        console.log('Authenticated successfully!');
+        request.session.user = userDB;
+        return response.send(200);
+    } else {
+        //console.log('Failed to Authenticate.')
+        return response.send(401)
+    }
 });
 
 router.post('/register', async (request, response) => {
-    const { username, password, email } = request.body;
-    const userDB = await User.findOne({ $or: [{ username }, { email }] });
+    const { email } = request.body;
+    const userDB = await User.findOne({ email });
     if (userDB) {
         response.status(400).send({ msg: 'User already existes!' });
     } else {
