e8631083a742c5be24c0e719b76bdd6ed6bb9292
ekow <ekow.a.indome@gmail.com>
2023-09-29 16:40:21 +0000

    trial using a hash map strategy for problem part 2

---
+++ Advent_of_Code/2020/Day-1/trials/hash-map-trial-4.py
@@ -0,0 +1,30 @@
+def find_three_numbers(nums, target):
+    nums.sort() # sort the list in ascending order
+    n = len(nums)
+
+    for i in range(n-2):
+        left = i + 1
+        right = n -1
+        while left < right:
+            current_sum = nums[i] + nums[left] + nums[right]
+            if current_sum == target:
+                # found a triplet that sums to the target
+                return [nums[i], nums[left], nums[right]]
+            elif current_sum < target:
+                left += 1
+            else:
+                right -= 1
+    
+    # if no triplet if found, return an empty list or handle it as needed
+    return []
+
+# example usage:
+def main():
+    expense_report = [1721, 979, 366, 299, 675, 1456]
+    target_sum = 2020
+    result = find_three_numbers(expense_report, target_sum)
+
+    if result:
+        product = result[0] * result[1]
+        print(f"Product: {product}")
+
