05fde9b729a03f835829ce1d43f90eb4346801d1
ekow <ekow.a.indome@gmail.com>
2023-09-30 22:46:10 +0000

    solution to day 3 part 1

---
+++ Advent_of_Code/2020/Day-3/solution/main.py
@@ -0,0 +1,40 @@
+def encounters(geological_map):
+    # characters
+    tree = "#"
+    open_square = "."
+
+    # slope parameters
+    right = 3
+    down  = 1
+
+    # encounter
+    encounter = 0
+
+    # current position
+    current_row = 0
+    current_col = 0
+
+    # traverse the map row by row
+    while current_row < len(geological_map):
+        # check for a tree
+        if geological_map[current_row][current_col] == tree:
+            encounter += 1
+        
+        # move right
+        current_col = (current_col+right) % len(geological_map[0])
+
+        # move down 
+        current_row += down
+    
+    return encounter
+
+def main():
+    with open("./input.txt", "r") as file:
+        map = [line.strip() for line in file]
+    
+    trees_encountered = encounters(map)
+
+    print(f"Encountered trees: {trees_encountered}")
+
+if __name__ == "__main__": 
+    main()
