a07ef2fd7689be88e086d61878ba65edd2575bcb
ekow <ekow.a.indome@gmail.com>
2023-10-03 12:05:21 +0000

    trial in solving day 5 part 2

---
+++ Advent_of_Code/2020/Day-5/trials/trial-4.py
@@ -0,0 +1,57 @@
+def find_highest_seat_ID(binary_spaces):
+    seat_IDs = []               # empty list to store seat IDs
+
+    print(f"Binary spaces b4: {binary_spaces}")
+    # iterate through space
+    for space in binary_spaces:
+        # intialize row range
+        row_low, row_high = 0, 127
+
+        # initialize column range
+        col_low, col_high = 0, 7
+
+        # iterate through the first 7 characters to find the row
+        for char in space[:7]:
+            mid = (row_low + row_high) // 2
+            if char == 'F':
+                row_high = mid
+            elif char == 'B':
+                row_low = mid + 1
+        #print()
+        #print(f"mid: {mid}, row_low: {row_low}, row_high: {row_high}")
+        # iterate throught the last 3 characters to find the column
+        for char in space[7:]:
+            mid = (col_low + col_high)//2
+            if char == 'L':
+                col_high = mid
+            elif char == 'R':
+                col_low = mid + 1
+        #print()
+        #print(f"mid: {mid}, col_low: {col_low}, col_high: {col_high}")
+
+        
+        seat_ID = (row_low * 8) + col_low     # calculate the seat ID
+        seat_IDs.append(seat_ID)            # add the id to the list
+        highest_seat_ID = max(seat_IDs)     # find the highest seat ID
+    print(f"List of seat IDs: {seat_IDs}")
+    sort_seats = sorted(seat_IDs)
+
+    # iterate through orted list
+    for i in range(len(sort_seats)-1):
+        # if the difference between the next seat and the current seat is greater than 1
+        if sort_seats[i+1] - sort_seats[i] > 1:
+            # found a gap in the sequence which indicates a missing seat
+            missing_seat_ID = sort_seats[i] + 1
+            return highest_seat_ID, missing_seat_ID
+
+
+def main():
+    with open("./input.txt", "r") as file:
+        binary_spaces = [line.strip() for line in file]
+    print(f"Binary spaces: {binary_spaces}")
+    highest_seat_ID, missing_seat_ID = find_highest_seat_ID(binary_spaces)
+    print(f"The highest seat ID is: {highest_seat_ID}\n"
+          f"The ID of your seat is: {missing_seat_ID}")
+
+if __name__ == "__main__":
+    main()
