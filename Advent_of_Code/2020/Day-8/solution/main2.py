4cc1dd8d2ee9db5732e801aaebb7e7897dc9651b
ekow <ekow.a.indome@gmail.com>
2023-10-06 11:35:51 +0000

    solution to problem part 2

---
+++ Advent_of_Code/2020/Day-8/solution/main2.py
@@ -0,0 +1,52 @@
+def find_acc_value(program):
+    acc = 0                         # accumulator
+    instruction_index = 0           # instruction index
+    executed_instruction = set()    # keep track of executed instructions
+
+    while instruction_index < len(program):
+        # check if instruction has been executed once
+        if instruction_index in executed_instruction:
+            break
+        
+        executed_instruction.add(instruction_index)      # if not, add to set to keep track
+        operation, argument = program[instruction_index] # unpack operation and argument of instruction
+        #instruction = program[instruction_index]
+
+        if operation == "acc":
+            acc += argument
+        elif operation == "jmp":
+            instruction_index += argument - 1 # subtract to account for loop increment
+
+        #print(f"Accumulator at instruction {instruction}:{instruction_index} = {acc}")
+
+        instruction_index += 1  # move to next instruction
+    
+    return acc
+
+def main():
+    with open("./input.txt", "r") as file:
+        instructions = [line.strip() for line in file]
+
+    program = []
+    for instruction in instructions:
+        operation, argument_str = instruction.split()
+        argument = int(argument_str)
+        program.append((operation, argument))
+
+    for i, (operation, argument) in enumerate(program):
+        if operation == "nop" or operation == "jmp":
+            modified_program = program.copy()
+
+            if operation == "nop":
+                modified_program[i] = ("jmp", argument)
+            elif operation == "jmp":
+                modified_program[i] - ("nop", argument)
+
+            result = find_acc_value(modified_program)
+            if result is not None:
+                print(f"Modified program with instruction {i} changed: Final accumulator value = {result}")
+                break
+    
+
+if __name__ == "__main__":
+    main()
