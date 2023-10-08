eed27c62d639bcf65d968041b4dfef430f16e71a
ekow <ekow.a.indome@gmail.com>
2023-10-08 22:12:24 +0000

    trial in solving problem part 2

---
+++ Advent_of_Code/2020/Day-10/trials/trial-5.py
@@ -0,0 +1,25 @@
+## Simplified code
+with open("./input.txt", "r") as file:
+    adapters = [int(line.strip()) for line in file]
+
+adapters.sort()     # sort the adapters in ascending orer
+adapters.insert(0, 0)  # add the charging outlet
+adapters.append(adapters[-1] + 3)  # add the built-in adapter
+
+# initialize a list to store the numbers of distinct arrangements
+# for each adapter
+arrangement_counts = [0] * len(adapters)
+#print(f"arrangement counts: {arrangement_counts}\n")
+
+arrangement_counts[0] = 1 # there's only one way to  the charging outlet
+#print(f"arrangement counts with charging outlet: {arrangement_counts}\n")
+
+# loop through the adapters and calculate the number of distinct arrangements
+for i in range(1, len(adapters)):
+    for j in range(i):
+        if adapters[i] - adapters[j] <= 3:
+            arrangement_counts[i] += arrangement_counts[j]
+
+# the final answer in the list is in the arrangement_counts[-1]
+total_arrangements = arrangement_counts[-1]
+print(f"Total distinct arrangements: {total_arrangements}")
