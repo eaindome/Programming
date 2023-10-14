a39412a71f04c670aaff45b38cabde62668c4ab0
ekow <ekow.a.indome@gmail.com>
2023-10-14 23:12:54 +0000

    trial 5, correct answer in solving problem part 1

---
+++ Advent_of_Code/2020/Day-15/trials/trial-5.py
@@ -0,0 +1,21 @@
+# simplifying the code for efficiency
+starting_numbers = [3,1,2]
+numbers = starting_numbers.copy()
+
+turn = len(starting_numbers) # start with the turn after the last starting number
+last_spoken = starting_numbers[-1]
+spoken_turns = {number: i + 1 for i, number in enumerate(starting_numbers[:-1])}
+
+while turn < 2020:          # continue the game until the 2020th turn
+    if last_spoken in spoken_turns:
+        age = turn - spoken_turns[last_spoken]
+        spoken_turns[last_spoken] = turn
+        last_spoken = age
+    else:
+        spoken_turns[last_spoken] = turn
+        last_spoken = 0
+    numbers.append(last_spoken)
+    turn += 1
+
+result = numbers[-1]
+print(f"The 2020th number spoken is: {result}")
