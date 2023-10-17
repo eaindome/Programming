bb0c6837063a009707eeef07b671a7a3e4b874c6
ekow <ekow.a.indome@gmail.com>
2023-10-17 19:33:47 +0000

    updating login so as to use passport authentication

--- NodeJS/ExpressJs/src/routes/auth.js
+++ NodeJS/ExpressJs/src/routes/auth.js
@@ -1,24 +1,30 @@
 const { Router } = require('express');
+const passport = require('passport');
 const User = require('../database/schemas/user_schemas');
 const { hashPassword, comparePassword } = require('./helpers');
 
 const router = Router();
 
 
-router.post('/login', async (request, response) => {
-    const { email, password } = request.body;
-    if (!email || !password) return response.status(400);
-    const userDB = await User.findOne({ email })
-    if (!userDB) return response.send(401);
-    const isValid = comparePassword(password, userDB.password);
-    if (isValid) {
-        console.log('Authenticated successfully!');
-        request.session.user = userDB;
-        return response.send(200);
-    } else {
-        //console.log('Failed to Authenticate.')
-        return response.send(401)
-    }
+// router.post('/login', async (request, response) => {
+//     const { email, password } = request.body;
+//     if (!email || !password) return response.status(400);
+//     const userDB = await User.findOne({ email })
+//     if (!userDB) return response.send(401);
+//     const isValid = comparePassword(password, userDB.password);
+//     if (isValid) {
+//         console.log('Authenticated successfully!');
+//         request.session.user = userDB;
+//         return response.send(200);
+//     } else {
+//         //console.log('Failed to Authenticate.')
+//         return response.send(401)
+//     }
+// });
+
+router.post('./login', passport.authenticate('local'), (req, res) => {
+    console.log('Logged In');
+    res.send(200);
 });
 
 router.post('/register', async (request, response) => {
