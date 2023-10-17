88045ce667d52eac7228a0a047fc5b83c9335520
ekow <ekow.a.indome@gmail.com>
2023-10-17 20:28:47 +0000

    implementing session stores

--- NodeJS/ExpressJs/src/index.js
+++ NodeJS/ExpressJs/src/index.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
+const passport = require('passport');
+const MongoStore = require('connect-mongo');
+// routes
 const groceriesRoute = require('./routes/groceries');
 const marketsRoute = require('./routes/markets');
 const authRoute = require('./routes/auth');
+
+// database initialization
 require('./database/init_db')
 
 const app = express();
 const PORT = 3001;
-
+// const memoryStore = new session.MemoryStore();
 
 
 app.use(express.json());
@@ -19,6 +24,10 @@ app.use(session({
     secret: 'your-secret',
     resave: false,
     saveUninitialized: false,
+    store: MongoStore.create({
+        mongoUrl: 'mongodb://127.0.0.1:27017/expressjs_tutorial',
+    }),
+
 }));
 
 app.use((req, res, next) => {
@@ -33,6 +42,9 @@ app.use((req, res, next) => {
         res.send(401);
     }
 });*/
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/api/v1/groceries', groceriesRoute);
 app.use('/api/v1/markets', marketsRoute);
