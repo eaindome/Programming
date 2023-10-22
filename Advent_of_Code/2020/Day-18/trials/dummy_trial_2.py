7f746978475e0bf101ce7d325c44772b50a2f39b
ekow <ekow.a.indome@gmail.com>
2023-10-22 23:31:58 +0000

    dummy trial 2 in testing 1 expression

---
+++ Advent_of_Code/2020/Day-18/trials/dummy_trial_2.py
@@ -0,0 +1,64 @@
+def evaluate_expression(expression):
+    # Replace spaces in the expression to make it easier to work with
+    expression = expression.replace(" ", "")
+
+    def evaluate_helper(expression):
+        stack = []
+        operators = []
+
+        i = 0
+
+        while i < len(expression):
+            if expression[i].isdigit():
+                # Extract the entire number (supports multi-digit numbers)
+                num = ""
+                while i < len(expression) and expression[i].isdigit():
+                    num += expression[i]
+                    i += 1
+                stack.append(int(num))
+            elif expression[i] in "+-*/":
+                while operators and operators[-1] in "+-*/":
+                    stack.append(operators.pop())
+                operators.append(expression[i])
+                i += 1
+            elif expression[i] == "(":
+                operators.append(expression[i])
+                i += 1
+            elif expression[i] == ")":
+                while operators[-1] != "(":
+                    stack.append(operators.pop())
+                operators.pop()
+                i += 1
+            else:
+                i += 1
+
+        while operators:
+            stack.append(operators.pop())
+
+        print(f"Stack: {stack}")
+        return stack
+
+    postfix_expression = evaluate_helper(expression)
+
+    stack = []
+
+    for item in postfix_expression:
+        if isinstance(item, int):
+            stack.append(item)
+        elif item in "+-*/":
+            b = stack.pop()
+            a = stack.pop()
+            if item == '+':
+                stack.append(a + b)
+            elif item == '-':
+                stack.append(a - b)
+            elif item == '*':
+                stack.append(a * b)
+            elif item == '/':
+                stack.append(a / b)
+
+    return stack[0]
+
+expression = '1 + (2 * 3) + (4 * (5 + 6))'
+result = evaluate_expression(expression)
+print(f"The result of the expression is: {result}")
