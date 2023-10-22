40df9fea52292326086ccea20a01345962fba098
ekow <ekow.a.indome@gmail.com>
2023-10-22 23:49:24 +0000

    trial for problem part 2

---
+++ Advent_of_Code/2020/Day-18/trials/trial-4.py
@@ -0,0 +1,61 @@
+with open("./input.txt", "r") as file:
+    expressions = [line.strip() for line in file] 
+
+all_result = []
+
+for expression in expressions:
+    i = 0
+    stack = []
+    operators = []
+
+    expression = expression.replace(" ", "")
+    #print(expression)
+    
+    while i < len(expression):
+        if expression[i].isdigit():
+            num = ""
+            while i < len(expression) and expression[i].isdigit():
+                num += expression[i]
+                i += 1
+            stack.append(int(num))
+        elif expression[i] in "+-*/":
+            while (
+                operators
+                and operators[-1] in "+-"
+                and (expression[i] in "*/")
+            ):
+                stack.append(operators.pop())
+            operators.append(expression[i])
+            i += 1
+        elif expression[i] == "(":
+            operators.append(expression[i])
+            i += 1
+        elif expression[i] == ")":
+            while operators[-1] != "(":
+                stack.append(operators.pop())
+            operators.pop()
+            i += 1
+        else:
+            i += 1
+
+    while operators:
+        stack.append(operators.pop())
+    
+    result = []
+    for item in stack:
+        if isinstance(item, int):
+            result.append(item)
+        elif item in "+-*/":
+            b = result.pop()
+            a = result.pop()
+            if item == '+':
+                result.append(a + b)
+            elif item == '-':
+                result.append(a - b)
+            elif item == '*':
+                result.append(a * b)
+            elif item == '/':
+                result.append(a / b)
+
+    print(f"Expression {expression} = {result[0]}")
+    all_result.append(result[0])
