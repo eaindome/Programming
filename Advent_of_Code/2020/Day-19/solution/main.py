58c73d73301c8e6028aece2b842f27907a22435f
ekow <ekow.a.indome@gmail.com>
2023-10-23 19:51:03 +0000

    solution to problem part 1

---
+++ Advent_of_Code/2020/Day-19/solution/main.py
@@ -0,0 +1,53 @@
+def is_valid_message(rules, message):
+    def match_rule(rule_num, text):
+        if isinstance(rules[rule_num], str):
+            if text.startswith(rules[rule_num]):
+                return text[len(rules[rule_num]):]
+            else:
+                return None
+        else:
+            for option in rules[rule_num]:
+                rest = text
+                for sub_rule in option:
+                    rest = match_rule(sub_rule, rest)
+                    if rest is None:
+                        break
+                if rest is not None:
+                    return rest
+            return None
+
+    return match_rule(0, message) == ''
+
+def main():
+    # read the rules and messages from input text file
+    with open("./input.txt", "r") as file:
+        lines = file.read().splitlines()
+
+    # parse the rules and messages
+    rules = {}
+    messages = []
+    parsing_rules = True
+
+    for line in lines:
+        if line == "":
+            parsing_rules = False
+        elif parsing_rules:
+            rule_num, rule_def = line.split(": ")
+            if '"' in rule_def:
+                rules[int(rule_num)] = rule_def.strip('"')
+            else:
+                sub_rules = [list(map(int, option.split())) for option in rule_def.split(" | ")]
+                rules[int(rule_num)] = sub_rules
+        else:
+            messages.append(line)
+
+    count = 0
+    for message in messages:
+        if is_valid_message(rules, message):
+            count += 1
+            # print(f"'{message}' is a valid message.")
+
+    print(f"Number of valid messages: {count}")
+
+if __name__ == "__main__":
+    main()
