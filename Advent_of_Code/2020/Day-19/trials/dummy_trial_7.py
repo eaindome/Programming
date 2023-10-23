754f9655ac529b4f85ffcad58c8adcf66c9da5e8
ekow <ekow.a.indome@gmail.com>
2023-10-23 20:50:44 +0000

    dummy trials for part 2

---
+++ Advent_of_Code/2020/Day-19/trials/dummy_trial_7.py
@@ -0,0 +1,74 @@
+def is_valid_message(rules, message):
+    def match_rule(rule_num, text):
+        if isinstance(rules[rule_num], str):
+            if text.startswith(rules[rule_num]):
+                return text[len(rules[rule_num]):]
+            else:
+                return None
+        else:
+            for option in rules[rule_num]:
+                rest = text
+                for sub_rule in option:
+                    rest = match_rule(sub_rule, rest)
+                    if rest is None:
+                        break
+                if rest is not None:
+                    return rest
+            return None
+
+    return match_rule(0, message) == ''
+
+def count_valid_messages(rules, messages):
+    def exclusively_matches_rule_0(message):
+        valid = is_valid_message(rules, message)
+        if valid:
+            for rule_num in rules:
+                if rule_num != 0:
+                    if is_valid_message(rules, message) is not None:
+                        return False
+        return valid
+
+    count = sum(1 for message in messages if exclusively_matches_rule_0(message))
+    return count
+
+def generate_modified_rules():
+    modified_rules = {
+        8: [[42], [42, 8]],
+        11: [[42, 31], [42, 11, 31]]
+    }
+    return modified_rules
+
+# Read the rules and messages from an input text file
+with open("./input2.txt", "r") as file:
+    lines = file.read().splitlines()
+
+# Check if we need to generate modified rules
+# Check if the original rules 8 and 11 are in the input
+use_modified_rules = "8: 42" in lines and "11: 42 31" in lines
+if use_modified_rules:
+    modified_rules = generate_modified_rules()
+    # Replace rules 8 and 11 with the modified rules
+    lines = [line if line != "8: 42" else "8: 42 | 42 8" for line in lines]
+    lines = [line if line != "11: 42 31" else "11: 42 31 | 42 11 31" for line in lines]
+
+# Parse the rules and messages
+rules = {}
+messages = []
+parsing_rules = True
+
+for line in lines:
+    if line == "":
+        parsing_rules = False
+    elif parsing_rules:
+        rule_num, rule_def = line.split(": ")
+        if '"' in rule_def:
+            rules[int(rule_num)] = rule_def.strip('"')
+        else:
+            sub_rules = [list(map(int, option.split())) for option in rule_def.split(" | ")]
+            rules[int(rule_num)] = sub_rules
+    else:
+        messages.append(line)
+
+# Count how many messages exclusively match rule 0
+valid_count = count_valid_messages(rules, messages)
+print(f"Number of valid messages exclusively matching rule 0: {valid_count}")
