965df73c7021254f0f8aeaafdad9bf50307f763b
ekow <ekow.a.indome@gmail.com>
2023-10-25 22:57:03 +0000

    Encapsulation and Data Hiding in Python

---
+++ Object Oriented Programming/learning/classes_and_objects/learning/py/ecpl.py
@@ -0,0 +1,23 @@
+# Encapsulation and Data Hiding
+
+class Computer:
+    def __init__(self):
+        self.__maxprice = 900
+
+    def sell(self):
+        print(f"Selling Price: {format(self.__maxprice)}")
+
+    def setMaxPrice(self, price):
+        self.__maxprice = price
+
+c = Computer()
+c.sell()
+
+# change the price
+c.__maxprice = 1000
+c.sell()
+
+# using  setter function
+c.setMaxPrice(1000)
+c.sell()
+
