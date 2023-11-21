656121c06c762e58085ff4e546c5340a1529e8f7
ekow <ekow.a.indome@gmail.com>
2023-11-21 17:36:46 +0000

    dummy trial to solve problem part 2

---
+++ Advent_of_Code/2020/Day-21/trials/dummy_trial_3.1.py
@@ -0,0 +1,69 @@
+class Food:
+    def __init__(self, ingredients, allergens):
+        self.ingredients = set(ingredients)
+        self.allergens = set(allergens)
+
+class AllergenAssessment:
+    def __init__(self, foods):
+        self.foods = foods
+        self.allergen_mapping = {}  # To store mapping of allergen to ingredient
+
+    def find_safe_ingredients(self):
+        all_ingredients = set()
+        for food in self.foods:
+            all_ingredients.update(food.ingredients)
+
+        possible_allergen_ingredients = set()
+        for allergen in set(allergen for food in self.foods for allergen in food.allergens):
+            foods_with_allergen = [food for food in self.foods if allergen in food.allergens]
+            allergen_ingredients = set.intersection(*(food.ingredients for food in foods_with_allergen))
+            possible_allergen_ingredients.update(allergen_ingredients)
+
+        safe_ingredients = all_ingredients - possible_allergen_ingredients
+        return safe_ingredients
+
+    def identify_allergen_ingredients(self):
+        max_iterations = 100  # Add a maximum number of iterations
+        iteration = 0
+
+        while len(self.allergen_mapping) < len(set(allergen for food in self.foods for allergen in food.allergens)) and iteration < max_iterations:
+            iteration += 1
+            for allergen in set(allergen for food in self.foods for allergen in food.allergens) - set(self.allergen_mapping.keys()):
+                foods_with_allergen = [food for food in self.foods if allergen in food.allergens]
+                possible_ingredients = set.intersection(*(food.ingredients for food in foods_with_allergen)) - set(self.allergen_mapping.values())
+
+                if len(possible_ingredients) == 1:
+                    ingredient = possible_ingredients.pop()
+                    self.allergen_mapping[allergen] = ingredient
+
+    def count_non_allergen_ingredients(self):
+        safe_ingredients = self.find_safe_ingredients()
+        count = sum(len(food.ingredients.intersection(safe_ingredients)) for food in self.foods)
+        return count
+
+    def get_canonical_dangerous_ingredient_list(self):
+        # Sort the allergen mapping by allergen name
+        sorted_allergen_mapping = sorted(self.allergen_mapping.items())
+        
+        # Get the ingredient for each allergen
+        ingredients = [ingredients for allergen, ingredients in sorted_allergen_mapping]
+        
+        # Join the ingredients with commas
+        canonical_dangerous_ingredient_list = ','.join(ingredients)
+        
+        #return canonical_dangerous_ingredient_list
+        return ingredients
+        
+foods_input = [
+    Food(["mxmxvkd", "kfcds", "sqjhc", "nhms"], ["dairy", "fish"]),
+    Food(["trh", "fvjkl", "sbzzf", "mxmxvkd"], ["dairy"]),
+    Food(["sqjhc", "fvjkl"], ["soy"]),
+    Food(["sqjhc", "mxmxvkd", "sbzzf"], ["fish"]),
+]
+
+allergen_assessment = AllergenAssessment(foods_input)
+allergen_assessment.identify_allergen_ingredients()
+canonical_dangerous_ingredient_list = allergen_assessment.get_canonical_dangerous_ingredient_list()
+
+print("Part 1:", allergen_assessment.count_non_allergen_ingredients())
+print("Part 2:", canonical_dangerous_ingredient_list)
