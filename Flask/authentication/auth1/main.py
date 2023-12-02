034a49fb10187a79be1d1a3401ba848bf6315be8
ekow <ekow.a.indome@gmail.com>
2023-12-02 11:39:54 +0500

    main flask application

---
+++ Flask/authentication/auth1/main.py
@@ -0,0 +1,64 @@
+from auth import auth_bp
+from extensions import db, jwt
+from flask import Flask, jsonify
+from models import User, TokenBlocklist
+from users import user_bp
+
+def create_app():
+    app = Flask(__name__)
+    
+    app.config.from_prefixed_env()  # 
+    
+    # initialize app
+    db.init_app(app=app) 
+    jwt.init_app(app)
+    
+    # register blueprint
+    app.register_blueprint(auth_bp, url_prefix='/auth') 
+    app.register_blueprint(user_bp, url_prefix='/users')
+    
+    
+    # load user
+    @jwt.user_lookup_loader
+    def user_lookup_callback(jwt_header, jwt_data):
+        identity = jwt_data['sub']
+        
+        return User.query.filter_by(username = identity).one_or_none()
+    
+    # additional claims
+    @jwt.additional_claims_loader
+    def make_additional_claims(identity):
+        if identity == "johndoe123":
+            return {"is_staff": True}
+        return {"is_staff": False}
+    
+    # jwt error handlers
+    @jwt.expired_token_loader
+    def expired_token_callback(jwt_header, jwt_data):
+        return jsonify({
+            "message": "Token has expired",
+            "error": "token_expired"
+        }), 401
+        
+    @jwt.invalid_token_loader
+    def invalid_token_callback(error):
+        return jsonify({
+            "message": "Signature verification failed",
+            "error": "invalid_token"
+        }), 401
+        
+    @jwt.unauthorized_loader
+    def missing_token_callback(error):
+        return jsonify({
+            "message": "Request does not contain a token",
+            "error": "authorization required"
+        }), 401
+    
+    @jwt.token_in_blocklist_loader
+    def token_in_blocklist_callback(jwt_header, jwt_data):
+        jti = jwt_data['jti']
+        
+        token = db.session.query(TokenBlocklist).filter(TokenBlocklist.jti == jti).scalar()
+        return token is not None
+        
+    return app
