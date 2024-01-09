41abe360f5fd6adb13b69ca2ff2c9f57918feee9
ekow <ekow.a.indome@gmail.com>
2024-01-09 08:31:55 +0500

    initializing python module

---
+++ Flask/projects/logBookSystem/server/app/routers/__init__.py
@@ -0,0 +1,9 @@
+# app/routes/__init__.py
+
+from .auth_routes import auth_bp
+from .user_routes import user_bp
+from .vendor_routes import vendor_bp
+from .staff_routes import staff_bp
+from .password_reset_routes import password_reset_bp
+from .forticlient_routes import forticlient_bp
+from .log_routes import log_bp
