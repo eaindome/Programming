20528c580a4990c04f7f9a738bdf5ca86725ce1d
ekow <ekow.a.indome@gmail.com>
2024-01-17 12:42:19 +0000

    token generation sample code

---
+++ Flask/projects/logBookSystem/server/app/authentication/token_generator.py
@@ -0,0 +1,14 @@
+from itsdangerous import URLSafeTimedSerializer
+from flask import current_app
+
+def generate_token(email):
+    serializer = URLSafeTimedSerializer(current_app.config['SECRET_KEY'])
+    return serializer.dumps(email, salt=current_app.config['SECURITY_PASSWORD_SALT'])
+
+def verify_token(token, max_age=3600):
+    serializer = URLSafeTimedSerializer(current_app.config['SECRET_KEY'])
+    try:
+        email = serializer.loads(token, salt=current_app.config['SECURITY_PASSWORD_SALT'], max_age=max_age)
+        return email
+    except:
+        return None
