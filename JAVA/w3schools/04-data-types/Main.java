81ed101ec3607f3cadceb0d2faa31cfdcc93c363
ekow <ekow.a.indome@gmail.com>
2024-01-20 20:43:45 +0000

    Data types in Java

---
+++ JAVA/w3schools/04-data-types/Main.java
@@ -0,0 +1,32 @@
+public class Main {
+    public static void main(String[] args) {
+        // other data types
+        int myInt = 9;
+        float myFloatNum = 5.99f;
+        char myLetter = 'D';
+        boolean myBool = true;
+        String myText = "Hello";
+
+        System.out.println("myInt = "+myInt+"\n"+"myFloatNum = "+myFloatNum+"\n"+"myLetter = "+myLetter+"\n"+"myBool = "+myBool+"\n"+"myText = "+myText+"\n");
+
+        // declare Multiple Variables
+        int x = 5, y = 6, z = 50;
+        System.out.println(x + y + z);
+
+        // one value to multiple variables
+        int a, b, c;
+        a = b = c = 25;
+        System.out.println(a + b + c);
+
+        // identifiers
+        int minutesPerHour = 60;
+        System.out.println("Using understable identifiers; minutes per hour: " + minutesPerHour);
+
+        // ascii values of characters
+        char myVar1 = 65, myVar2 = 66, myVar3 = 67;
+        System.out.println(myVar1);
+        System.out.println(myVar2);
+        System.out.println(myVar3);
+        System.out.println();  
+    }
+}
