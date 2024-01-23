972da7906dfbae497f51f84c970a3f0add47ecee
ekow <ekow.a.indome@gmail.com>
2024-01-23 19:08:57 +0000

    Exercise 5

---
+++ JAVA/practice/codesDope/01-let's program/05/Main.java
@@ -0,0 +1,11 @@
+/*
+ * Write a program to print the difference and product of the numbers 
+ * 45 and 32.
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Difference of 45 and 32: " + (45-32));
+        System.out.println("Product of 45 and 32: " + (45*32));
+    }
+}
