e7a51268e0535dbccd1cd171b21bfb0b334f8706
ekow <ekow.a.indome@gmail.com>
2024-01-23 19:10:03 +0000

    Exercise 3

---
+++ JAVA/practice/codesDope/02-know datatypes/03/Main.java
@@ -0,0 +1,9 @@
+/*
+ * Write a program to print the product of the numbers 8.2 and 6.
+*/
+public class Main {
+    public static void main(String[] args) {
+        double product = 8.2 * 6;
+        System.out.println("Product of 8.2 and 6: "+product);
+    }
+}
