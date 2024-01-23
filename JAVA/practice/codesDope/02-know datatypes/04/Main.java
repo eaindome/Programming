11ec9078002e98c31a627955367c21e1a9c79a32
ekow <ekow.a.indome@gmail.com>
2024-01-23 19:10:13 +0000

    Exercise 4

---
+++ JAVA/practice/codesDope/02-know datatypes/04/Main.java
@@ -0,0 +1,9 @@
+/*
+ * Print the ASCII value of the character 'h'.
+*/
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("ASCII value of \'h\': " + ((int)'h'));
+    }
+}
