50c2120b66c7b7bfb5c0529dd54623420e297b0d
ekow <ekow.a.indome@gmail.com>
2024-01-23 19:11:12 +0000

    Exercise 8

---
+++ JAVA/practice/codesDope/02-know datatypes/08/Main.java
@@ -0,0 +1,10 @@
+/*
+ * Write a program to find the square of the number 3.9.
+*/
+
+public class Main {
+    public static void main(String[] args) {
+        double sqre = 3.9 * 3.9;
+        System.out.println("Square of 3.9: " + sqre);
+    }
+}
