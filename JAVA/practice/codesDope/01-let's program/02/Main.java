8e1cc1a9dfdf3a4bf03de7037f357ed0cfacdf50
ekow <ekow.a.indome@gmail.com>
2024-01-23 19:07:59 +0000

    Exercise 2

---
+++ JAVA/practice/codesDope/01-let's program/02/Main.java
@@ -0,0 +1,19 @@
+/*
+ * Write a program to print
+    *
+    **
+    ***
+    ****
+    on screen.
+*/
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("""
+                *
+                **
+                ***
+                ****
+                """);
+    }
+}
