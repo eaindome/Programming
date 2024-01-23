926e4d8fbae2edb724b61d723c1536f33387fbab
ekow <ekow.a.indome@gmail.com>
2024-01-23 19:08:43 +0000

    Exercise 4

---
+++ JAVA/practice/codesDope/01-let's program/04/Main.java
@@ -0,0 +1,9 @@
+/*
+ * Write a program to print the sum of the numbers 2, 4 and 5.
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Sum of 2, 4 and 5: " + (2 + 4 + 5));
+    }
+}
