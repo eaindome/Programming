e29b64362fd55d433b8800b96070d89b1835de8a
ekow <ekow.a.indome@gmail.com>
2024-01-23 19:10:49 +0000

    Exercise 6

---
+++ JAVA/practice/codesDope/02-know datatypes/06/Main.java
@@ -0,0 +1,11 @@
+/*
+ * Write a program to add 3 to the ASCII value of the character 'd' 
+ * and print the equivalent character.
+*/
+
+public class Main {
+    public static void main(String[] args) {
+        int value = 3 + ((int)'d');
+        System.out.println("3 + ASCII value of \'d\': "+value);
+    }
+}
