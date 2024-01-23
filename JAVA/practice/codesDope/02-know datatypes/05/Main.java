aee36dbe00dae4c5d5d082cae6a41d749202d72d
ekow <ekow.a.indome@gmail.com>
2024-01-23 19:10:39 +0000

    Exercise 5

---
+++ JAVA/practice/codesDope/02-know datatypes/05/Main.java
@@ -0,0 +1,12 @@
+/*
+ * Write a program to assign a value of 100.235 
+ * to a double variable and then convert it to int.
+*/
+
+public class Main {
+    public static void main(String[] args) {
+        double var = 100.235;
+        int var2 = (int) var;
+        System.out.println("Double "+var+" into integer: "+var2); 
+    }
+}
