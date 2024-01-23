5a3bd9276191cc83104e9b877ce98fb696f00e06
ekow <ekow.a.indome@gmail.com>
2024-01-23 19:08:34 +0000

    Exercise 3

---
+++ JAVA/practice/codesDope/01-let's program/03/Main.java
@@ -0,0 +1,20 @@
+/*
+ * Print the following pattern on the screen
+    *****
+     *** 
+      *  
+     *** 
+    *****
+*/
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("""
+                *****
+                 ***
+                  *
+                 ***
+                *****
+                """);
+    }
+}
