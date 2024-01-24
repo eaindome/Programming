4f5b80f28a029822537af5fe2876ef2527a82af6
ekow <ekow.a.indome@gmail.com>
2024-01-24 21:16:29 +0000

    Exercise 7

---
+++ JAVA/practice/codesDope/03-java operators/07/Main.java
@@ -0,0 +1,11 @@
+/*
+ * Assign values of variables 'a' and 'b' as 55 and 70 respectively 
+ * and then check if both the conditions 'a < 50' and 'a < b' are true.
+ */
+public class Main {
+    public static void main(String[] args) {
+        int a = 55, b = 70;
+        System.out.println("Is 55 < 50: " + (a < 50));
+        System.out.println("Is 55 < 70: " + (a < b));
+    }
+}
