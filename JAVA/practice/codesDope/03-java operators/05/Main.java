e4359a6f020dd70be045e1309a1aa8d04d3b9611
ekow <ekow.a.indome@gmail.com>
2024-01-24 21:16:10 +0000

    Exercise 5

---
+++ JAVA/practice/codesDope/03-java operators/05/Main.java
@@ -0,0 +1,9 @@
+/*
+ * Write a program to check if the two numbers 23 and 45 are equal.
+*/
+public class Main {
+    public static void main(String[] args) {
+        int num1 = 23, num2 = 45;
+        System.out.println("Is 23 equal to 45: " + (num1==num2));
+    }
+}
