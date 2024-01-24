be218883d34ae6c20031343e58037fc15e84a787
ekow <ekow.a.indome@gmail.com>
2024-01-24 21:15:38 +0000

    Exercise 2

---
+++ JAVA/practice/codesDope/03-java operators/02/Main.java
@@ -0,0 +1,12 @@
+/*
+ * Write a program to calculate the perimeter of a triangle 
+ * having sides of length 2,3 and 5 units.
+*/
+
+public class Main {
+    public static void main(String[] args) {
+        int side1 = 2, side2 = 3, side3 = 5, perimeter;
+        perimeter = side1 + side2 + side3;
+        System.out.println("Perimeter of triangle: " + perimeter);
+    }
+}
