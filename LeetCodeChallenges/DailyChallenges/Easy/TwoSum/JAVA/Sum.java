e375954f21f5c331998532ddc6991d8f3b43d3a2
eaindome <ekow.a.indome@gmail.com>
2024-06-04 23:59:28 +0000

    another solution in JAVA

---
+++ LeetCodeChallenges/DailyChallenges/Easy/TwoSum/JAVA/Sum.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Sum {
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> numMap = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (numMap.containsKey(complement)) {
+                return new int[] {numMap.get(complement), i};
+            }
+            numMap.put(nums[i], i);
+        }
+
+        throw new IllegalArgumentException("No two sum solution found.");
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = twoSum(nums, target);
+        System.out.println(Arrays.toString(result));
+    }
+}
