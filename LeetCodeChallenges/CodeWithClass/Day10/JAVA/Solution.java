9724bc3d60ce82323a9a2be1a013f95917052805
eaindome <ekow.a.indome@gmail.com>
2024-06-26 03:15:38 +0000

    Solution 1

---
+++ LeetCodeChallenges/CodeWithClass/Day10/JAVA/Solution.java
@@ -0,0 +1,24 @@
+
+public class Solution {
+    public String convertToBase7(int num) {
+        if (num == 0) {
+            return "0";
+        }
+        StringBuilder strDigits = new StringBuilder();
+        int numAbs = Math.abs(num);
+        while (numAbs > 0) {
+            strDigits.append(numAbs % 7);
+            numAbs /= 7;
+        }
+        if (num < 0) {
+            strDigits.append("-");
+        }
+        return strDigits.reverse().toString();
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.convertToBase7(100));  // Output: "202"
+        System.out.println(solution.convertToBase7(-7));  // Output: "-10"
+    }
+}
