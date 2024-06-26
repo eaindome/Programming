d9c0d8db6a3cf4ca352089866d8d9d0d61e5b0cb
eaindome <ekow.a.indome@gmail.com>
2024-06-26 23:59:56 +0000

    4. Toggle completion of tasks

--- BlockChain/ToDoList/contracts/TodoList.sol
+++ BlockChain/ToDoList/contracts/TodoList.sol
@@ -22,6 +22,11 @@ contract TodoList {
         bool completed
     );
 
+    event TaskCompleted(
+        uint id,
+        bool completed
+    );
+
     // constructor function that is executed once when the contract is deployed
     constructor() public {
         // automatically create an initial task when the contract is deployed
@@ -33,5 +38,12 @@ contract TodoList {
         taskCount ++;                                           // increment the taskCount by 1
         tasks[taskCount] = Task(taskCount, _content, false);    // add the new task to the mapping with the new taskCount as the key
         emit TaskCreated(taskCount, _content, false);
+    }
+
+    function toggleCompleted(uint _id) public {
+        Task memory _task = tasks[_id];     // retrieve the task from the mapping
+        _task.completed = !_task.completed;  // toggle the completed status of the task
+        tasks[_id] = _task;                  // update the task in the mapping
+        emit TaskCompleted(_id, _task.completed);   // emit an event to notify the client that the task has been completed
     }
 }
