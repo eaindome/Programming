2a839bdc6f6fdfc4fc828129e5e806fd44312893
eaindome <ekow.a.indome@gmail.com>
2024-07-23 10:06:38 +0000

    too many changes

--- NodeJS/custom-modules-trial/mathOperations.js
+++
@@ -1,7 +0,0 @@
-// mathOperations.js
-module.exports = {
-    add: (a, b) => a + b,
-    subtract: (a, b) => a - b,
-    multiply: (a, b) => a * b,
-    divide: (a, b) => (b !== 0 ? a / b : 'Cannot divide by zero.'),
-};
