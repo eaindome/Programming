6489a1105f5ec7d0b919003830b509c5518692b6
eaindome <ekow.a.indome@gmail.com>
2024-08-12 01:19:02 +0000

    Article table structure

--- Backend-Projects/01-Personal_Blogging_Platform_API/models/Articles.js
+++ Backend-Projects/01-Personal_Blogging_Platform_API/models/Articles.js
@@ -1,22 +1,36 @@
 const { DataTypes } = require('sequelize');
-const { sequelize } = require('../config/database');
+const { sequelize } = require('../config/database'); // Adjust the path as needed
 
-
-const Article = sequelize.define(
-    'Article', {
-        title: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        content: {
-            type: DataTypes.TEXT,
-            allowNull: false,
-        },
-        published: {
-            type: DataTypes.BOOLEAN,
-            defaultValue: false,
-        },
-    }
-);
+const Article = sequelize.define('Article', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  content: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+  published: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false,
+  },
+  createdAt: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    defaultValue: DataTypes.NOW,
+  },
+  updatedAt: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    defaultValue: DataTypes.NOW,
+  },
+}, {
+  tableName: 'Articles', // Specify the table name explicitly
+});
 
 module.exports = Article;
