a04636a7a6fdbf7ee993f4890db3cf4b1fae7165
ekow <ekow.a.indome@gmail.com>
2024-09-08 16:01:22 +0000

    Operators in JavaScript

--- JavaScript/beginners_crash_course/main.js
+++ JavaScript/beginners_crash_course/main.js
@@ -43,10 +43,10 @@ const isNewUser= false;
 
 // Undefined
 let result
-console.log(result);
+// console.log(result);
 
 const res = undefined;
-console.log(res);
+// console.log(res);
 
 // Null
 const data = null;
@@ -62,10 +62,50 @@ const person = {
     afilliation: 'The Justice League of America'
 };
 
-console.log(`Name: ${person.firstName} ${person.lastName}.\nAlias: ${person.alias}.\nAge: ${person.age}.\nAffliation: ${person.afilliation}`);
+// console.log(`Name: ${person.firstName} ${person.lastName}.\nAlias: ${person.alias}.\nAge: ${person.age}.\nAffliation: ${person.afilliation}`);
 
 // Arrays
 const oddNumbers = [1, 3, 5, 7, 9];
-console.log(oddNumbers[0]);
+// console.log(oddNumbers[0]);
 
+// OPERATORS
+// 1. Assignment Operators -> '='
+let x = 10;
+let y = 2;
+
+// 2. Arithmetic Operators
+console.log(x + y);
+console.log(x * y);
+console.log(x - y);
+console.log(x / y);
+console.log(x % y);
+console.log(++x);
+console.log(--y);
+
+// 3. Comparison Operators
+console.log(x == y);
+console.log(x != y);
+console.log(x === y);
+console.log(x !== y);
+console.log(x > y);
+console.log(x >= y);
+console.log(x < y);
+console.log(x <= y);
+
+// 4. Logical Operators
+let isValidNumber;
+isValidNumber = x > 8 && 8 > y;
+console.log(`isValidNumber: ${isValidNumber}`);
+isValidNumber = x > 8 || 8 > y;
+console.log(`isValidNumber: ${isValidNumber}`);
+
+console.log(`{is New User ${!isNewUser}`);
+console.log(`is Primary Number: ${!isPrimaryNumber}`);
+
+// 5. String Operators
+console.log('Bruce ' + 'Wayne');        // addition with numbers, concatenation with strings
+
+// 6. Other Operators
+const isEven = 10 % 2 === 0 ? true : false;     // tenary operators
+console.log(`is Even number: ${isEven}`);
 
