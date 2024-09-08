18555a44b04180e67bbdd214cbaca7256012cca7
ekow <ekow.a.indome@gmail.com>
2024-09-08 15:00:02 +0000

    Variables in javascript

--- JavaScript/beginners_crash_course/main.js
+++ JavaScript/beginners_crash_course/main.js
@@ -1,2 +1,13 @@
 // log a statement using console.log()
 console.log('Hello World!');
+
+/**
+ * VARIABLES
+ * 1. let - values for let variables can be re-assigned
+ * 2. const - all const declarations must be initialized, and once initialized, can't be reassigned
+*/
+let age = 25;
+console.log(`Age: ${age}\n`);
+
+const salary = 1200
+console.log(`Salary: ${salary}`);
