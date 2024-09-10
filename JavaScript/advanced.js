51efea5fdde950adb694732e0fffb32bf6b8454f
ekow <ekow.a.indome@gmail.com>
2024-09-10 14:15:51 +0000

    Closures

--- JavaScript/advanced.js
+++ JavaScript/advanced.js
@@ -2,16 +2,43 @@
  * NESTED FUNCTION'S SCOPE
  * 
 */
-let a = 10;                     // global scope
-function outer () {         // outer function scope
-    let b = 20;                 // function based scope
-    function inner() {      // inner function scope
-        let c = 30;
-        console.log(a, b, c);   // nested function scope
+// let a = 10;                     // global scope
+// function outer () {         // outer function scope
+//     let b = 20;                 // function based scope
+//     function inner() {      // inner function scope
+//         let c = 30;
+//         console.log(a, b, c);   // nested function scope
+//     }
+//     inner();
+// }
+// outer()
+
+/**
+ * CLOSURES
+ *    - the combination of a function bundled together with references to its surrounding state
+ *    - created every time a function is created i.e. at function creation time
+ * 
+ *    * Other definition
+ *      - when we return a function from another function, we are 
+ *        effectively returning a combination of the function definition along with the function's scope.
+ *        This would let the function definition have an associated persisten memory which could hold on 
+ *        to live data between executions. That combination of the functino and its scope chain is what is
+ *        called a closuer in JavaScript.
+*/
+function outer() {
+    let counter = 0;
+    function inner() {
+        counter++;
+        // console.log(`counter: ${counter}`);
     }
-    inner();
+    return inner;
 }
-outer()
+const fun = outer();
+fun();
+fun();
+ 
+
+
 
 
 
