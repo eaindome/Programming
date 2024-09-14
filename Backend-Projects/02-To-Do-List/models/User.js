868f067e060d64b4e19e082ca15f0114f9d271c3
ekow <ekow.a.indome@gmail.com>
2024-09-14 13:04:31 +0000

    define relationship between users and tasks

--- Backend-Projects/02-To-Do-List/models/User.js
+++ Backend-Projects/02-To-Do-List/models/User.js
@@ -1,6 +1,7 @@
 const { DataTypes } = require('sequelize');
 const { sequelize }= require('../config/database');
 const bcrypt = require('bcryptjs');
+const Task = require('./Tasks');
 
 const User = sequelize.define('User', {
     username: {
@@ -19,5 +20,9 @@ const User = sequelize.define('User', {
 User.prototype.validPassword = async function (password) {
     return await bcrypt.compare(password, this.password);
 };
+
+// define relationships between users and tasks
+User.hasMany(Task, { foreignKey: 'userId' });
+Task.belongsTo(User, { foreignKey: 'userId' });
 
 module.exports = User;
