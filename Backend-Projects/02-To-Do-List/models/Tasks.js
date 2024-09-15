1424e54221f9084f9419b5e208626592082ccc76
ekow <ekow.a.indome@gmail.com>
2024-09-15 05:55:09 +0000

    adding various status to the task table, status column

--- Backend-Projects/02-To-Do-List/models/Tasks.js
+++ Backend-Projects/02-To-Do-List/models/Tasks.js
@@ -12,7 +12,7 @@ const Task = sequelize.define('Task', {
         allowNull: true,
     },
     status: {
-        type: DataTypes.STRING,
+        type: DataTypes.ENUM('pending', 'completed'),
         defaultValue: 'pending',
     },
     userId: {
