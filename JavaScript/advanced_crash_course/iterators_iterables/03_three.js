134d9756c81001c6185ad5c6392f44f1d649d297
ekow <ekow.a.indome@gmail.com>
2024-09-21 16:54:00 +0000

    Scenario 3

---
+++ JavaScript/advanced_crash_course/iterators_iterables/03_three.js
@@ -0,0 +1,10 @@
+/**
+ * Scenario 3: Write a function that takes an iterable 
+ * (e.g., an array) and logs each item using a for..of loop.
+ */
+
+const evenNum = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20];
+
+for (const num of evenNum) {
+    console.log(`Even number: ${num}`);
+}
