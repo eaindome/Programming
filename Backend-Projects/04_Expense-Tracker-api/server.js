b3ed144ac4a3969e579e9cc02f1b7934ba432df9
ekow <ekow.a.indome@gmail.com>
2024-09-25 20:07:02 +0000

    server and database configuration

--- Backend-Projects/04_Expense-Tracker-api/server.js
+++ Backend-Projects/04_Expense-Tracker-api/server.js
@@ -6,10 +6,7 @@ dotenv.config();
 
 const PORT = process.env.PORT ?? 5000;
 
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParse: true,
-    useUnifiedTopology: true
-})
+mongoose.connect(process.env.MONGO_URI)
     .then(() => {
         console.log('Connected to MongoDB');
         app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
