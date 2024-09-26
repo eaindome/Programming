13e5db245121a0477e7909bce547eb2a76213a6d
ekow <ekow.a.indome@gmail.com>
2024-09-26 09:39:28 +0000

    Unit testing: Registration Endpoint

--- Backend-Projects/04_Expense-Tracker-api/tests/auth.test.js
+++ Backend-Projects/04_Expense-Tracker-api/tests/auth.test.js
@@ -1,6 +1,11 @@
 const request = require('supertest');
+const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+
+const User = require('../models/userModel');
 const app = require('../app');
+
+dotenv.config();
 
 beforeAll(async () => {
     await mongoose.connect(process.env.MONGO_URI);
@@ -11,16 +16,22 @@ afterAll(async () => {
     await mongoose.connection.close();
 });
 
+afterEach(async () => {
+    // clean up the test data after each test
+    await User.deleteMany({});
+});
+
 describe('Registration Process', () => {
     test(
         'POST /api/auth/register should register a new user successfully',
         async () =>{
             const newUser = {
-                username: 'TestUser',
-                password: 'test@password'
+                username: 'TestUser1',
+                password: 'test1@password'
             };
 
             const response = await request(app).post('/api/auth/register').send(newUser);
+            // console.log(`Response: ${JSON.stringify(response)}`);
             expect(response.status).toBe(201);
             expect(response.body.message).toBe('User registered successfully.');
         }
@@ -41,6 +52,7 @@ describe('Registration Process', () => {
             };
 
             const response = await request(app).post('/api/auth/register').send(duplicateUser);
+            // console.log(`Response: ${response}`);
             expect(response.status).toBe(400);
             expect(response.body.message).toBe('User already exists.')
         }
@@ -55,6 +67,7 @@ describe('Registration Process', () => {
             };
 
             const response = await request(app).post('/api/auth/register').send(incompleteUser);
+            // console.log(`Response: ${response}`);
             expect(response.status).toBe(400);
             expect(response.body.message).toBe('Username and password required.');
         }
