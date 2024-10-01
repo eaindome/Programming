83ad9200be387efbc7a724aeb899e733db062820
ekow <ekow.a.indome@gmail.com>
2024-10-01 12:37:12 +0000

    Node.js-13-Module Caching 1

--- NodeJS/NodeJS_Course/start/index.js
+++ NodeJS/NodeJS_Course/start/index.js
@@ -7,5 +7,15 @@
 // const sum2 = add(sum, 3);
 // console.log(`Sum: ${sum}\nSum2: ${sum2}`);
 
-require('./batman');
-require('./superman');
+// require('./batman');
+// require('./superman');
+
+const superHero = require('./superHero');
+console.log(superHero.getName());
+
+superHero.setName('Superman');
+console.log(superHero.getName());
+
+
+const newSuperHero = require('./superHero');
+console.log(newSuperHero.getName());
