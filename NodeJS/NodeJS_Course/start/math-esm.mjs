0c20f7526651e2d40c5c6be07e804ec4a33312f6
ekow <ekow.a.indome@gmail.com>
2024-10-01 14:06:59 +0000

    Node.js-16-ES Modules 3

--- NodeJS/NodeJS_Course/start/math-esm.mjs
+++ NodeJS/NodeJS_Course/start/math-esm.mjs
@@ -1,5 +1,12 @@
-export default (a, b) => {
+const add = (a, b) => {
     return a + b;
 };
 
-// export default add;
+const subtract = (a, b) => {
+    return a - b;
+};
+
+export default {
+    add,
+    subtract 
+};
