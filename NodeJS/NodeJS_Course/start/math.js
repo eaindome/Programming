98b498a045863286a11f2b77f47af3bdf96b7d69
ekow <ekow.a.indome@gmail.com>
2024-10-01 12:45:41 +0000

    Node.js-14-Import Export Patterns 5

--- NodeJS/NodeJS_Course/start/math.js
+++ NodeJS/NodeJS_Course/start/math.js
@@ -1,12 +1,12 @@
-module.exports.add = (a, b) => {
+exports.add = (a, b) => {
     return a + b;
 };
 
-module.exports.subtract = (a, b) => {
+exports.subtract = (a, b) => {
     return a - b;
 };
 
 // module.exports = {
 //     add,
 //     subtract
-// };
+// }; 
