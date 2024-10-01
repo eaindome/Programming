10710ee1d0e5fb744f63cbbca8b3eb11ee3ea6cb
ekow <ekow.a.indome@gmail.com>
2024-10-01 14:22:14 +0000

    Node.js-17-Importing JSON

--- NodeJS/NodeJS_Course/start/index.js
+++ NodeJS/NodeJS_Course/start/index.js
@@ -25,8 +25,12 @@
 // superman.setName('Clark Kent.');
 // console.log(superman.getName());
 
-const math = require('./math');
+// const math = require('./math');
 
-const { add, subtract } = math;
-console.log(add(2, 3));
-console.log(subtract(2, 3));
+// const { add, subtract } = math;
+// console.log(add(2, 3));
+// console.log(subtract(2, 3));
+
+const data = require('./data.json');    // recommend .json xtension 
+
+console.log(`Data: ${data}`);
