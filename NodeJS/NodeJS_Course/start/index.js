ed6c62ab14c2a9817734f6fc4ab9a880cb5914ea
ekow <ekow.a.indome@gmail.com>
2024-10-02 16:46:37 +0000

    Node.js-27-fs Promise Module

--- NodeJS/NodeJS_Course/start/index.js
+++ NodeJS/NodeJS_Course/start/index.js
@@ -170,3 +170,26 @@ const path = require("node:path");
 
 //     console.log('Successfully written to file!')
 // });
+
+
+// fs promise module
+const fs = require('node:fs/promises');
+
+// console.log('First\n');
+// fs.readFile('file.txt', 'utf-8')
+//     .then(data => console.log(data))
+//     .catch((err) => console.log(err));
+
+// console.log('Second\n');
+
+// async function readFile() {
+//     try {
+//         const data = await fs.readFile('file.txt', 'utf-8');
+//         console.log(`Data:\n${data}`);
+//     } catch (err) {
+//         console.error(`Error: ${err}`);
+//         return '';
+//     }
+// }
+
+// readFile();
