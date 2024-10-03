f8a881d79a089ce10ae335736363dbcac7989c1e
ekow <ekow.a.indome@gmail.com>
2024-10-03 16:05:25 +0000

    Node.js-35-HTTP Routing

--- NodeJS/NodeJS_Course/start/index.js
+++ NodeJS/NodeJS_Course/start/index.js
@@ -310,3 +310,39 @@ const path = require("node:path");
 // server.listen(PORT, () => {
 //     console.log(`Server running on port: ${PORT}`);
 // });
+
+
+
+// HTTP Routing
+// const http = require("node:http");
+// const fs = require("node:fs");
+
+// const server = http.createServer((req, res) => {
+//        if (req.url === '/') {
+//         res.writeHead(200, {
+//             "Content-Type": "text/plain"
+//         });
+//         res.end("Home page");
+//     } else if (req.url === '/about') {
+//         res.writeHead(200, {
+//             "Content-Type": "text/plain"
+//         });
+//         res.end("About page")
+//     } else if (req.url === '/api') {
+//         res.writeHead(200, {
+//             "Content-Type": "application/json"
+//         });
+//         res.end(JSON.stringify({
+//             firstName: "Bruce",
+//             lastName: "Wayne"
+//         }));
+//     } else {
+//         res.writeHead(404);
+//         res.end("Page not found")
+//     }
+// });
+
+// const PORT = 3000;
+// server.listen(PORT, () => {
+//     console.log(`Server running on port: ${PORT}`);
+// });
