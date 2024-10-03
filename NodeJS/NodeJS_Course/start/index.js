cc4d89e5202ab963d26b969278f78f56724c2e73
ekow <ekow.a.indome@gmail.com>
2024-10-03 15:42:29 +0000

    Node.js-34-HTML Template

--- NodeJS/NodeJS_Course/start/index.js
+++ NodeJS/NodeJS_Course/start/index.js
@@ -291,22 +291,22 @@ const path = require("node:path");
 
 
 // HTML Template
-const http = require("node:http");
-const fs = require("node:fs");
+// const http = require("node:http");
+// const fs = require("node:fs");
 
-const server = http.createServer((request, response) => {
-    const name = 'Kay24';
-    response.writeHead(200, {
-        "Content-Type": "text/html"
-    });
-    let html = fs.readFileSync('index.html', 'utf-8');
+// const server = http.createServer((request, response) => {
+//     const name = 'Kay24';
+//     response.writeHead(200, {
+//         "Content-Type": "text/html"
+//     });
+//     let html = fs.readFileSync('index.html', 'utf-8');
 
-    html = html.replace("{{name}}", name);
+//     html = html.replace("{{name}}", name);
     
-    response.end(html);
-});
+//     response.end(html);
+// });
 
-const PORT = 3000;
-server.listen(PORT, () => {
-    console.log(`Server running on port: ${PORT}`);
-});
+// const PORT = 3000;
+// server.listen(PORT, () => {
+//     console.log(`Server running on port: ${PORT}`);
+// });
