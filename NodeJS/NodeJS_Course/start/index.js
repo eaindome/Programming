ee6154faafd52de7fe9d2874dc3d5cc5bb7d044e
ekow <ekow.a.indome@gmail.com>
2024-10-03 17:08:59 +0000

    Node.js-40-Thread Pool Size

--- NodeJS/NodeJS_Course/start/index.js
+++ NodeJS/NodeJS_Course/start/index.js
@@ -368,3 +368,22 @@ const path = require("node:path");
 
 // every method in node.js that has the 'sync' suffix always runs on the
 // main thread and is blocking
+
+
+// Thread Pool Size
+// const crypto = require('node:crypto');
+
+// process.env.UV_THREADPOOL_SIZE = 8;
+// const MAX_CALLS = 8;
+
+// const start = Date.now();
+// for (let i = 0; i < MAX_CALLS; i++) {
+//     crypto.pbkdf2('password', 'salt', 100000, 512, 'sha512', () => {
+//     console.log(`Hash: ${i + 1}`, Date.now() - start);
+//     });
+// }
+
+/**
+ * Increasing the thread pool size can help with performance
+ * but that is limited by the number of available CPU cores
+ */
