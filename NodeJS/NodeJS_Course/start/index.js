b89c50c82589cce89d70ea74069722f908cce142
ekow <ekow.a.indome@gmail.com>
2024-10-07 06:13:05 +0000

    Node.js-43-Microtask Queues

--- NodeJS/NodeJS_Course/start/index.js
+++ NodeJS/NodeJS_Course/start/index.js
@@ -419,3 +419,50 @@ const path = require("node:path");
  * of synchronous and asynchronous code in Node.js
  */
 
+// Microtask Queues
+// Ex 1
+// console.log("console.log 1");
+// process.nextTick(() => {
+//     console.log("this is process.next tick 1");
+// });
+// console.log("console.log 2");
+/**
+ * All user written synchronous Javascript code takes
+ * priority over async code
+ */
+
+// Ex 2
+// Promise.resolve().then(() => {
+//     console.log("this is Promise.resolve 1");
+// });
+// process.nextTick(() => {
+//     console.log("this is process.next tick 1");
+// });
+
+// Ex 3
+// process.nextTick(() => {
+//     console.log("this is process.next tick 1");
+// });
+// process.nextTick(() => {
+//     console.log("this is process.next tick 2");
+//     process.nextTick(() => {
+//         console.log("this is the inner next tick inside next tick.");
+//     });
+// });
+// process.nextTick(() => {
+//     console.log("this is process.next tick 3");
+// });
+
+
+// Promise.resolve().then(() => {
+//     console.log("this is Promise.resolve 1");
+// });
+// Promise.resolve().then(() => {
+//     console.log("this is Promise.resolve 2");
+//     process.nextTick(() => {
+//         console.log("this is the inenr next tick inside Promise then block");
+//     });
+// });
+// Promise.resolve().then(() => {
+//     console.log("this is Promise.resolve 3");
+// });
