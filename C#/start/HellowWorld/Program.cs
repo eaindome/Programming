2ce85df588d76a5290d2e93b67ff6c445ed7d477
ekow <ekow.a.indome@gmail.com>
2024-10-09 08:36:26 +0000

    Starting dotnet

---
+++ C#/start/HellowWorld/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+using System;
+using System.Linq;
+
+namespace Sample
+{
+    class Test
+    {
+        public static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3};
+
+            Console.WriteLine($"Smallest Elements: {numbers.Min()}");
+
+            Console.WriteLine($"Largest Element: {numbers.Max()}");
+
+            Console.ReadLine();
+        }
+    }
+}
+
+
