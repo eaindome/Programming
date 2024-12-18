7b458add8767b9928dfb1dfb594b3ab4f7e8bcf3
ekow <ekow.a.indome@gmail.com>
2024-12-18 10:59:01 +0000

    authentication validators

---
+++ Backend-Projects/10_RecipeShare/server/src/utils/validators.ts
@@ -0,0 +1,9 @@
+export const isValidEmail = (email: string) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+};
+
+export const isStrongPassword = (password: string) => {
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    return passwordRegex.test(password);
+};
