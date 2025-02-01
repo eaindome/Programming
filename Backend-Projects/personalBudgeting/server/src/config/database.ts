ca1633d60d80bbeede16925dac2b39424f399845
ekow <ekow.a.indome@gmail.com>
2025-02-01 11:17:15 +0000

    Database configuration

--- Backend-Projects/personalBudgeting/server/src/config/database.ts
+++ Backend-Projects/personalBudgeting/server/src/config/database.ts
@@ -1,11 +1,25 @@
-import mongoose from 'mongoose';
+import { MongoClient, Db } from 'mongodb';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const uri = process.env.MONGODB_URI || '';
+const client = new MongoClient(uri);
+
+let db: Db;
 
 export const connectDatabase = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI || '');
+        await client.connect();
+        db = client.db('personalBudgetingDB');
         console.log('Database connection successful');
     } catch (error) {
         console.error(`Database connection error: ${error}`);
         process.exit(1);
     }
 };
+
+export const getDB = (): Db => {
+    if (!db) throw new Error("Database not initialized.");
+    return db;
+}
