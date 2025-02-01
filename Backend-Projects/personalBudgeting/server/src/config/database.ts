d483b9c81e96ebf3adf94870e165f3b0234b2fd5
ekow <ekow.a.indome@gmail.com>
2025-02-01 10:54:40 +0000

    Database configuration

---
+++ Backend-Projects/personalBudgeting/server/src/config/database.ts
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose';
+
+export const connectDatabase = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI || '');
+        console.log('Database connection successful');
+    } catch (error) {
+        console.error(`Database connection error: ${error}`);
+        process.exit(1);
+    }
+};
