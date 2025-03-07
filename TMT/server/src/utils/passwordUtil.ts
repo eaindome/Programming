876ce71a63959156b7bca57f55cf1073a0b81acb
ekow <ekow.a.indome@gmail.com>
2025-03-07 00:01:14 +0000

    Authentication utilities

---
+++ TMT/server/src/utils/passwordUtil.ts
@@ -0,0 +1,9 @@
+export function isStrongPassword(password: string): boolean {
+    const minLength = 8;
+    const hasUpperCase = /[A-Z]/.test(password);
+    const hasLowerCase = /[a-z]/.test(password);
+    const hasNumbers = /\d/.test(password);
+    const hasSpecialChar = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/.test(password);
+    
+    return password.length >= minLength && hasUpperCase && hasLowerCase && hasNumbers && hasSpecialChar;
+}
