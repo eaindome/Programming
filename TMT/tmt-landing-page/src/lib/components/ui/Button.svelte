9d89ee18aa422c74e09f4b55857d0b4ec59b2264
ekow <ekow.a.indome@gmail.com>
2025-03-09 05:56:07 +0000

    Adding a navigation event dispatch

--- TMT/tmt-landing-page/src/lib/components/ui/Button.svelte
+++ TMT/tmt-landing-page/src/lib/components/ui/Button.svelte
@@ -11,6 +11,10 @@
         : 'bg-white hover:bg-gray-100 text-[#1A5F7A] shadow-lg hover:shadow-xl focus:ring-[#1A5F7A]'}
       ${size === 'sm' ? 'py-2 px-4 text-sm' : size === 'lg' ? 'py-4 px-8 text-lg' : 'py-3 px-6 text-base'}
     `;
+
+    // Handle click event
+    import { createEventDispatcher } from "svelte";
+    const dispatch = createEventDispatcher();
 </script>
   
 {#if href}
@@ -18,7 +22,7 @@
       <slot></slot>
     </a>
 {:else}
-    <button class={buttonClass}>
+    <button class={buttonClass} on:click={() => dispatch('click')}>
       <slot></slot>
     </button>
 {/if}
