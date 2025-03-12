a1ab22e574ac6a1dc31e58ca439ce783bc228e3b
ekow <ekow.a.indome@gmail.com>
2025-03-12 05:27:54 +0000

    Notification Models

---
+++ NotificationSystem/Models/Notification.cs
@@ -0,0 +1,24 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NotificationSystem.Models
+{
+    public class Notification
+    {
+        public int Id { get; set; }
+        public string? UserId { get; set; } // User identifier (could be email, username, or ID)
+        
+        [Required]
+        [MaxLength(500)]
+        public required string Message { get; set; }
+        
+        [MaxLength(50)]
+        public string? Type { get; set; } = "info";
+        public bool IsRead { get; set; } = false;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        [NotMapped]
+        public string Timestamp => CreatedAt.ToString("o"); // ISO 8601
+    }
+}
